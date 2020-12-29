dffa5046e5811826454822762e1006d00b716bf4
yuri014 <yuriflores2012@gmail.com>
2020-12-29 19:24:53 -0300

    adding landing html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,33 @@ import React from 'react';
 import Head from 'next/head';
 import { FaGamepad } from 'react-icons/fa';
 
+import StartIcon from '../assets/start-artist-icon.svg';
+import LandingContainer from '../styles/pages';
+
 const Landing: React.FC = () => (
-  <>
+  <LandingContainer>
     <Head>
       <title>Comp-Art</title>
     </Head>
     <div className="start">
-      <div id="start-content">
-        <div className="start-title">
-          Comp-
-          <br />
-          Art
-        </div>
-        <div className="start-text">
-          Rede social que visa superar os metódos conhecidos de divulgação
-          artística.
-        </div>
-        <div className="press-start">
-          Start
-          <FaGamepad />
-        </div>
+      <div className="start-title">
+        Comp-
+        <br />
+        Art
+      </div>
+      <div className="start-icon">
+        <StartIcon />
+      </div>
+      <div className="start-text">
+        Rede social que visa superar os metódos conhecidos de divulgação
+        artística.
+      </div>
+      <div className="press-start">
+        Start
+        <FaGamepad />
       </div>
     </div>
-  </>
+  </LandingContainer>
 );
 
 export default Landing;
