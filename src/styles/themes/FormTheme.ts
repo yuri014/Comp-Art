8ce07f057f254bfd75d94b54a795a5d937e883b7
yuri014 <yuriflores2012@gmail.com>
2020-12-30 00:22:37 -0300

    material ui form theme

---
+++ src/styles/themes/FormTheme.ts
@@ -0,0 +1,40 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const formTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1cc5b7',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: ['-apple-system', 'BlinkMacSystemFont', '"Asap"'].join(','),
+    fontSize: 18,
+  },
+  overrides: {
+    MuiInput: {
+      input: {
+        '&::placeholder': {
+          color: 'gray',
+        },
+        color: '#cbcaeb',
+      },
+    },
+    MuiTextField: {
+      root: {
+        marginBottom: '1rem',
+        '@media (min-width: 1100px)': {
+          marginBottom: '2rem',
+        },
+      },
+    },
+    MuiInputLabel: {
+      root: {
+        color: '#cbcaeb',
+      },
+    },
+  },
+});
+
+export default formTheme;
