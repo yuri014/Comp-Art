4c22cbe5b68a828a57f135595e1ce1a598e4b512
yuri014 <yuriflores2012@gmail.com>
2021-01-01 20:31:31 -0300

    mobile header

---
+++ src/components/MobileHeader/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import { FaCog } from 'react-icons/fa';
+
+import MobileHeaderContainer from './styles';
+
+const MobileHeader: React.FC = () => (
+  <MobileHeaderContainer>
+    <Link href="/profile">
+      <a>
+        <img
+          src="https://images.pexels.com/photos/3981624/pexels-photo-3981624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="Imagem de perfil"
+        />
+      </a>
+    </Link>
+    <Link href="/profile">
+      <a>
+        <div className="logo">
+          <h1>COMP-ART</h1>
+        </div>
+      </a>
+    </Link>
+    <Link href="/config">
+      <a>
+        <FaCog />
+      </a>
+    </Link>
+  </MobileHeaderContainer>
+);
+
+export default MobileHeader;
---
+++ src/components/MobileHeader/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const size = '24px';
+
+const MobileHeaderContainer = styled.header`
+  position: absolute;
+  top: 0;
+  font-size: ${size};
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${props => props.theme.colors.namesakeText};
+  padding: 1rem 2rem;
+
+  h1 {
+    font: 400 20px ${props => props.theme.fonts.display}, sans-serif;
+    filter: blur(0.4px);
+    letter-spacing: 4px;
+    color: #ffffff;
+    text-shadow: 3px 3px 2px ${props => props.theme.colors.titleColor};
+  }
+
+  img {
+    height: ${size};
+    width: ${size};
+    border-radius: 50%;
+  }
+
+  a {
+    height: ${size};
+    color: ${props => props.theme.colors.mainColor};
+  }
+
+  @media (min-width: 1100px) {
+    display: none;
+  }
+`;
+
+export default MobileHeaderContainer;
