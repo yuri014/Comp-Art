c9f2f405651e6567a33316e602c7a76b2db30f29
yuri014 <yuriflores2012@gmail.com>
2021-01-02 13:44:39 -0300

    home page styles

---
+++ src/styles/pages/home/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.main`
+  .home-desktop-content {
+    display: grid;
+    grid-template-columns: 25% 50% 25%;
+    justify-content: center;
+    padding: 2rem 4rem;
+  }
+`;
+
+export default HomeContainer;
