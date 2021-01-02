ea52ef8c8d97902e4791069efedbe007a9823fcc
yuri014 <yuriflores2012@gmail.com>
2021-01-02 13:44:30 -0300

    adding profile column to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import Head from 'next/head';
 
 import Header from '../../components/Header';
+import HomeContainer from '../../styles/pages/home';
+import HomeProfile from '../../components/HomeProfile';
 import MobileHeader from '../../components/MobileHeader';
 import MobileFooter from '../../components/MobileFooter';
 
 const Home: React.FC = () => (
-  <>
+  <HomeContainer>
     <Head>
       <title>Comp-Art</title>
     </Head>
     <Header />
+    <div className="home-desktop-content">
+      <HomeProfile />
+      <div>Timeline</div>
+      <div>BLANK</div>
+    </div>
     <MobileHeader />
     <MobileFooter />
-  </>
+  </HomeContainer>
 );
 
 export default Home;
