64a36b55da7092cdb4703a64daf79c6523047c32
yuri014 <yuriflores2012@gmail.com>
2021-01-02 11:00:12 -0300

    removing link to profile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NoSsr, TextField, ThemeProvider } from '@material-ui/core';
 import { FaBell, FaCog, FaScroll, FaSearch } from 'react-icons/fa';
-import Link from 'next/link';
 
 import HeaderContainer from './styles';
 import formTheme from '../../styles/themes/FormTheme';
@@ -26,12 +25,6 @@ const Header: React.FC = () => (
       </button>
     </div>
     <div className="header-icons">
-      <Link href="/profile">
-        <img
-          src="https://images.pexels.com/photos/3981624/pexels-photo-3981624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="Imagem de perfil"
-        />
-      </Link>
       <span>
         <FaBell />
       </span>
