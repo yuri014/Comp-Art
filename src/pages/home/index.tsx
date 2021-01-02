5f0ad4a589ff43f1ded58902c9596fe0489a73ab
yuri014 <yuriflores2012@gmail.com>
2021-01-02 00:52:29 -0300

    adding desktop header to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
 
-import MobileFooter from '../../components/MobileFooter';
+import Header from '../../components/Header';
 import MobileHeader from '../../components/MobileHeader';
+import MobileFooter from '../../components/MobileFooter';
 
 const Home: React.FC = () => (
   <>
     <Head>
       <title>Comp-Art</title>
     </Head>
+    <Header />
     <MobileHeader />
     <MobileFooter />
   </>
