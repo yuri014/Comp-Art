bc69f0210eff93ec28ed71b82aa6ca404c7ecdd1
yuri014 <yuriflores2012@gmail.com>
2021-01-02 11:52:29 -0300

    adding gradient and change secondary background color

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -3,9 +3,14 @@ const theme = {
     mainColor: '#1cc5b7',
     titleColor: '#50c878',
     backgroundColor: '#050211',
-    secondaryBackgroundColor: '#1c283b',
+    secondaryBackgroundColor: '#0f0f28',
     namesakeText: '#08162f',
     lightContrast: '#cbcaeb',
+    mainGradient: `linear-gradient(
+      310deg,
+      #050211 0%,
+      #141432 100%
+    )`,
   },
   fonts: {
     display: '"Audiowide"',
