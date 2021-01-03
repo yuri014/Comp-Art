aabb550a1c06232c29f1f9779e8a7ccb32d9a44a
yuri014 <yuriflores2012@gmail.com>
2021-01-02 21:46:47 -0300

    fix grid

--- src/styles/pages/home/index.ts
+++ src/styles/pages/home/index.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const HomeContainer = styled.main`
   .home-desktop-content {
     display: grid;
-    grid-template-columns: 25% 50% 25%;
-    justify-content: center;
+    grid-template-columns: 25% 45% 25%;
+    justify-content: space-between;
     padding: 2rem 4rem;
   }
 `;
