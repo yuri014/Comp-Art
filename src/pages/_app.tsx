9518b7d4aa3748041655ea6bf9c1455bb2b6d143
yuri014 <yuriflores2012@gmail.com>
2021-01-09 12:22:00 -0300

    adding apollo client and graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import GlobalStyle from '../styles/global';
 import theme from '../styles/themes/dark';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache(),
+});
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-    <GlobalStyle />
-  </ThemeProvider>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  </ApolloProvider>
 );
 
 export default MyApp;
