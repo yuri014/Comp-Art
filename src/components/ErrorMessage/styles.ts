a9f204ed2aced56053323c6f5b8952d1ecfef29a
yuri014 <yuriflores2012@gmail.com>
2021-01-09 17:06:25 -0300

    error component style

---
+++ src/components/ErrorMessage/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ErrorMessageContainer = styled.div`
+  color: #f50057;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 2rem;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 0.6rem;
+  border-radius: 4px;
+  border: 1px solid #f50057;
+`;
+
+export default ErrorMessageContainer;
