81f3bc61c9636835222e2933b631178d8aeb0d61
yuri014 <yuriflores2012@gmail.com>
2021-01-10 13:32:18 -0300

    adding auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/themes/dark';
+import { AuthProvider } from '../context/auth';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3333/graphql',
@@ -13,10 +14,12 @@ const client = new ApolloClient({
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </AuthProvider>
   </ApolloProvider>
 );
 
