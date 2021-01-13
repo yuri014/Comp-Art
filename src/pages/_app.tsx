ed925580a9a4bc9d7c30990d36f8e4401d8f4e52
yuri014 <yuriflores2012@gmail.com>
2021-01-12 23:38:40 -0300

    adding authorization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/themes/dark';
 import { AuthProvider } from '../context/auth';
 
+const httpLink = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_API_HOST}/graphql`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:3333/graphql',
   cache: new InMemoryCache(),
+  link: (authLink.concat(httpLink) as unknown) as ApolloLink,
 });
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
