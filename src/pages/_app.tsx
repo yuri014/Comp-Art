67d03eb6b5bcec878af26f008416b8efd94f854c
yuri014 <yuriflores2012@gmail.com>
2021-01-15 20:59:52 -0300

    adding nprogress loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,9 @@ import {
 } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 import { createUploadLink } from 'apollo-upload-client';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 import GlobalStyle from '../styles/global';
 import theme from '../styles/themes/dark';
@@ -32,6 +35,10 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: (authLink.concat(httpLink as never) as unknown) as ApolloLink,
 });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <ApolloProvider client={client}>
