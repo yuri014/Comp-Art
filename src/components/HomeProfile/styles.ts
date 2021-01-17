61f7ed4df6f3a4d676f6fd9f9229af4324a5632f
yuri014 <yuriflores2012@gmail.com>
2021-01-17 16:35:50 -0300

    removing hashtag styles

--- src/components/HomeProfile/styles.ts
+++ src/components/HomeProfile/styles.ts
@@ -99,27 +99,6 @@ const HomeProfileContainer = styled.aside`
       display: flex;
       align-items: center;
     }
-    & .hashtags {
-      display: flex;
-      justify-content: flex-start;
-      flex-wrap: wrap;
-      font-size: 12px;
-      & span {
-        margin-top: 2rem;
-        margin-right: 2rem;
-        cursor: pointer;
-        padding: 1rem;
-        background-color: ${props => props.theme.colors.lightContrast};
-        color: ${props => props.theme.colors.backgroundColor};
-        font-weight: 700;
-        border-radius: 4px;
-        text-shadow: 0 0 2px #00000050;
-        transition: all 0.4s ease;
-        &:hover {
-          filter: brightness(0.4);
-        }
-      }
-    }
   }
 
   @media (max-width: 1100px) {
