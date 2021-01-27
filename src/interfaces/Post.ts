16f0e5314d8692f66f71dd66b8a27ea275d321fc
yuri014 <yuriflores2012@gmail.com>
2021-01-27 20:29:09 -0300

    fix interface for post component

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -1,16 +1,22 @@
-export interface IPost {
-  getPosts: [
+interface Post {
+  description: string;
+  body: string;
+  likes: [
     {
-      description: string;
-      body: string;
-      likes: {
-        username: string;
-        avatar: string;
-        createdAt: string;
-      };
-      sharedCount: number;
+      username: string;
+      avatar: string;
       createdAt: string;
-      artist: string;
     },
   ];
+  sharedCount: number;
+  createdAt: string;
+  artist: string;
+}
+
+export interface PostProps {
+  post: Post;
+}
+
+export interface IPost {
+  getPosts: Array<Post>;
 }
