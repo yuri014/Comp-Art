92285df2ab6d03ef7af50383a56afbbb862b379a
yuri014 <yuriflores2012@gmail.com>
2021-01-27 00:29:23 -0300

    adding infinite scroll for pagination

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import Head from 'next/head';
+import { useQuery } from '@apollo/client';
 
 import Header from '../../components/Header';
 import HomeContainer from '../../styles/pages/home';
@@ -9,25 +10,72 @@ import MobileFooter from '../../components/MobileFooter';
 import Post from '../../components/Post';
 import QuestsProgress from '../../components/QuestsProgress';
 import withAuth from '../../hocs/withAuth';
+import GET_POSTS from '../../graphql/queries/post';
 
-const Home: React.FC = () => (
-  <HomeContainer>
-    <Head>
-      <title>Comp-Art</title>
-    </Head>
-    <Header />
-    <div className="home-desktop-content">
-      <HomeProfile />
-      <div className="timeline">
-        <Post />
+const Home: React.FC = () => {
+  const [hasMore, setHasMore] = useState(false);
+  const observer = useRef(null);
+  const { data, loading, error, fetchMore } = useQuery(GET_POSTS, {
+    variables: { offset: 0 },
+  });
+
+  const lastPostRef = useCallback(
+    node => {
+      if (!data.getPosts) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && !hasMore) {
+          fetchMore({
+            variables: { offset: data.getPosts.length },
+          }).then(fetchMoreResult => {
+            if (fetchMoreResult.data.getPosts.length < 3) {
+              setHasMore(true);
+            }
+          });
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [data],
+  );
+
+  if (loading) return <p>loading</p>;
+
+  if (error) return <p>error</p>;
+
+  return (
+    <HomeContainer>
+      <Head>
+        <title>Comp-Art</title>
+      </Head>
+      <Header />
+      <div className="home-desktop-content">
+        <HomeProfile />
+        <div className="timeline">
+          {data.getPosts.map((_, index) => {
+            if (data.getPosts.length === index + 1) {
+              return (
+                <div ref={lastPostRef}>
+                  <Post />
+                </div>
+              );
+            }
+            return (
+              <div>
+                <Post />
+              </div>
+            );
+          })}
+        </div>
+        <div className="quests">
+          <QuestsProgress />
+        </div>
       </div>
-      <div className="quests">
-        <QuestsProgress />
-      </div>
-    </div>
-    <MobileHeader />
-    <MobileFooter />
-  </HomeContainer>
-);
+      <MobileHeader />
+      <MobileFooter />
+    </HomeContainer>
+  );
+};
 
 export default withAuth(Home);
