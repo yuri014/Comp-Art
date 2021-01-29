bfc38296a503fd6f434abf2b0ce63b5df229255a
yuri014 <yuriflores2012@gmail.com>
2021-01-29 00:31:15 -0300

    empty post component

---
+++ src/components/Post/Empty.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import { FiInfo } from 'react-icons/fi';
+import { FaRegCompass } from 'react-icons/fa';
+
+import { EmptyPostContainer } from './styles';
+
+const EmptyPost: React.FC = () => (
+  <EmptyPostContainer>
+    <FiInfo size={80} />
+    <p>Não há posts por enquanto!</p>
+    <p>Você ainda pode explorar para achar coisas que possam te emocionar!</p>
+    <Link href="/explore">
+      <a>
+        <button type="button">
+          Explorar&nbsp;
+          <span>
+            <FaRegCompass size={20} />
+          </span>
+        </button>
+      </a>
+    </Link>
+  </EmptyPostContainer>
+);
+
+export default EmptyPost;
