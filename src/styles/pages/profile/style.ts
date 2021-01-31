03b2b405933bdc0c443fdc2da258ca7b52ef9921
yuri014 <yuriflores2012@gmail.com>
2021-01-31 11:41:19 -0300

    fix css scope

--- src/styles/pages/profile/style.ts
+++ src/styles/pages/profile/style.ts
@@ -53,76 +53,78 @@ const ProfileContainer = styled.div`
     }
   }
 
-  & section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem 3rem;
-
-    .profile {
-      width: 100%;
+  main {
+    & section {
       display: flex;
+      flex-direction: column;
       align-items: center;
-      justify-content: space-between;
-      & h1 {
-        font-size: 20px;
-      }
+      padding: 1rem 3rem;
 
-      & h2 {
-        font-size: 14px;
-        color: ${props => props.theme.colors.lightText};
-      }
+      .profile {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        & h1 {
+          font-size: 20px;
+        }
 
-      & p {
-        font-size: 14px;
-        color: ${props => props.theme.colors.lightText};
-      }
+        & h2 {
+          font-size: 14px;
+          color: ${props => props.theme.colors.lightText};
+        }
 
-      & p:first-child {
-        font-size: 20px;
-        font-weight: 600;
-        color: ${props => props.theme.colors.lightContrast};
-        & .level {
-          background-color: ${props => props.theme.colors.mainColor};
-          color: ${props => props.theme.colors.backgroundColor};
-          padding: 0 0.8rem;
-          border-radius: 4px;
+        & p {
+          font-size: 14px;
+          color: ${props => props.theme.colors.lightText};
+        }
+
+        & p:first-child {
+          font-size: 20px;
+          font-weight: 600;
+          color: ${props => props.theme.colors.lightContrast};
+          & .level {
+            background-color: ${props => props.theme.colors.mainColor};
+            color: ${props => props.theme.colors.backgroundColor};
+            padding: 0 0.8rem;
+            border-radius: 4px;
+          }
         }
       }
-    }
 
-    .bio {
-      margin-top: 2rem;
-      width: 100%;
-      font-size: 14px;
-      border: 1px solid ${props => props.theme.colors.namesakeText};
-      border-radius: 4px;
-      padding: 1rem;
-    }
+      .bio {
+        margin-top: 2rem;
+        width: 100%;
+        font-size: 14px;
+        border: 1px solid ${props => props.theme.colors.namesakeText};
+        border-radius: 4px;
+        padding: 1rem;
+      }
 
-    .mobile-profile-follows {
-      width: 100%;
-      margin-top: 2rem;
-      font-size: 16px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-    .profile-links {
-      width: 100%;
-      margin-top: 2rem;
-      font-size: 22px;
-      display: grid;
-      grid-template-columns: 15% 15% 15% 15%;
-      align-items: flex-start;
+      .profile-follows {
+        display: none;
+      }
+
+      .mobile-profile-follows {
+        width: 100%;
+        margin-top: 2rem;
+        font-size: 16px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+      .profile-links {
+        width: 100%;
+        margin-top: 2rem;
+        font-size: 22px;
+        display: grid;
+        grid-template-columns: 15% 15% 15% 15%;
+        align-items: flex-start;
+      }
     }
   }
 
   @media (min-width: 1100px) {
-    & main {
-      padding-top: 6rem;
-    }
-
     .edit-profile {
       width: 100%;
       display: flex;
@@ -144,80 +146,84 @@ const ProfileContainer = styled.div`
       margin-top: -4rem;
     }
 
-    & section {
-      padding: 1rem 4rem;
-      display: grid;
-      margin-top: -10.4rem;
-      grid-template-columns: 35% 45% 35%;
-      grid-template-areas:
-        'profile profile .'
-        'bio bio links';
-      align-items: baseline;
+    main {
+      padding-top: 6rem;
 
-      .profile {
-        margin-left: 16rem;
-        display: flex;
+      & section {
+        padding: 1rem 4rem;
+        display: grid;
+        margin-top: -10.4rem;
+        grid-template-columns: 35% 45% 35%;
+        grid-template-areas:
+          'profile profile .'
+          'bio bio links';
         align-items: baseline;
-        justify-content: flex-start;
-        grid-area: profile;
-        & h1 {
-          font-size: 24px;
-        }
 
-        & h2 {
-          font-size: 16px;
-          color: ${props => props.theme.colors.lightText};
-          margin-top: 0.4rem;
-        }
+        .profile {
+          margin-left: 16rem;
+          display: flex;
+          align-items: baseline;
+          justify-content: flex-start;
+          grid-area: profile;
+          & h1 {
+            font-size: 24px;
+          }
 
-        & p:first-child {
-          font-size: 24px;
-          margin-top: 0;
-        }
+          & h2 {
+            font-size: 16px;
+            color: ${props => props.theme.colors.lightText};
+            margin-top: 0.4rem;
+          }
 
-        & p {
-          font-size: 16px;
-          margin-left: 6rem;
-          margin-top: 0.4rem;
-          font-weight: 700;
-          & .level {
-            margin-left: 0.8rem;
-            padding: 0 1rem !important;
+          & p:first-child {
+            font-size: 24px;
+            margin-top: 0;
+          }
+
+          & p {
+            font-size: 16px;
+            margin-left: 6rem;
+            margin-top: 0.4rem;
+            font-weight: 700;
+            & .level {
+              margin-left: 0.8rem;
+              padding: 0 1rem !important;
+            }
           }
         }
-      }
 
-      .mobile-profile-follows {
-        display: none;
-      }
-
-      .profile-follows {
-        margin-top: 2rem;
-        display: block;
-        font-size: 18px;
-        & p:first-child {
-          margin-bottom: 0.4rem;
-          font-size: 18px;
+        .mobile-profile-follows {
+          display: none;
         }
 
-        & p {
+        .profile-follows {
+          margin-top: 2rem;
+          display: block;
           font-size: 18px;
-          color: ${props => props.theme.colors.lightContrast};
+          & p:first-child {
+            margin-bottom: 0.4rem;
+            font-size: 18px;
+          }
+
+          & p {
+            font-size: 18px;
+            color: ${props => props.theme.colors.lightContrast};
+          }
         }
-      }
 
-      .bio {
-        font-size: 16px;
-        width: 80%;
-        grid-area: bio;
-      }
+        .bio {
+          font-size: 16px;
+          width: 80%;
+          grid-area: bio;
+        }
 
-      .profile-links {
-        font-size: 28px;
-        grid-area: links;
-        margin-top: 4rem;
-        & a {
-          text-align: right;
+        .profile-links {
+          font-size: 28px;
+          grid-area: links;
+          margin-top: 4rem;
+          & a {
+            text-align: right;
+          }
         }
       }
     }
