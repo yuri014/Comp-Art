0f7a83f87f7adf6596f66a24d36d5a8a0a6e1f6d
yuri014 <yuriflores2012@gmail.com>
2021-02-07 15:52:49 -0300

    separating skeleton from loading

--- src/components/Post/LoadingPost.tsx
+++ src/components/Post/LoadingPost.tsx
@@ -1,76 +1,14 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
-import {
-  FaCog,
-  FaRegComment,
-  FaRegHeart,
-  FaRegShareSquare,
-} from 'react-icons/fa';
-import Skeleton from '@material-ui/lab/Skeleton';
 
-import { PostContainer } from './styles';
-import mainTheme from '../../styles/themes/MainTheme';
 import EmptyPost from './Empty';
+import SkeletonPost from './SkeletonPost';
 
 interface LoadingProps {
   loading: boolean;
 }
 
 const LoadingPost: React.FC<LoadingProps> = ({ loading }) => (
-  <>
-    {loading ? (
-      <PostContainer>
-        <div className="post-author">
-          <div className="author-info">
-            <Skeleton
-              animation="wave"
-              variant="circle"
-              width={60}
-              height={60}
-            />
-            <div>
-              <Skeleton animation="wave" width={150} height={24} />
-              <span>
-                <Skeleton
-                  animation="wave"
-                  width={120}
-                  height={24}
-                  component="p"
-                />
-              </span>
-            </div>
-          </div>
-          <div className="post-config">
-            <FaCog />
-          </div>
-        </div>
-        <div className="post">
-          <div className="post-description">
-            <Skeleton animation="wave" width="100%" height={60} />
-          </div>
-          <Skeleton animation="wave" variant="rect" width="100%" height={478} />
-          <ThemeProvider theme={mainTheme}>
-            <div className="post-interaction">
-              <div>
-                <FaRegHeart />
-                <p>Favoritar</p>
-              </div>
-              <div>
-                <FaRegComment />
-                <p>Comentar</p>
-              </div>
-              <div>
-                <FaRegShareSquare />
-                <p>Compartilhar</p>
-              </div>
-            </div>
-          </ThemeProvider>
-        </div>
-      </PostContainer>
-    ) : (
-      <EmptyPost />
-    )}
-  </>
+  <>{loading ? <SkeletonPost /> : <EmptyPost />}</>
 );
 
 export default LoadingPost;
