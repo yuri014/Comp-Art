c454e9fb8122ba247c5eeea5fb40b759e9a523dd
yuri014 <yuriflores2012@gmail.com>
2021-02-07 19:06:16 -0300

    adding viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from '@apollo/client';
+import Head from 'next/head';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
@@ -18,14 +19,19 @@ Router.events.on('routeChangeError', () => NProgress.done());
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <AuthProvider>
-      <ApolloProvider client={apolloClient}>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-          <GlobalStyle />
-        </ThemeProvider>
-      </ApolloProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <AuthProvider>
+        <ApolloProvider client={apolloClient}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+            <GlobalStyle />
+          </ThemeProvider>
+        </ApolloProvider>
+      </AuthProvider>
+    </>
   );
 };
 
