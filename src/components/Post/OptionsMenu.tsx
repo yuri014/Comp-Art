e8e6d77df94c8cf5b7ec9d432d429f7b487a021c
yuri014 <yuriflores2012@gmail.com>
2021-02-09 21:34:53 -0300

    options menu post

---
+++ src/components/Post/OptionsMenu.tsx
@@ -0,0 +1,52 @@
+import React, { memo, useContext, useState } from 'react';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { FiMoreVertical, FiTrash2 } from 'react-icons/fi';
+
+import { AuthContext } from '../../context/auth';
+
+interface OptionsMenuProps {
+  username: string;
+}
+
+const OptionsMenu: React.FC<OptionsMenuProps> = ({ username }) => {
+  const auth = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton
+        aria-controls="menu-post"
+        aria-haspopup="true"
+        onClick={handleClick}
+        color="secondary"
+      >
+        <FiMoreVertical />
+      </IconButton>
+      <Menu
+        id="menu-post-image"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>Copiar Link</MenuItem>
+        {auth.user && auth.user.username === username && (
+          <MenuItem>
+            <span className="danger-icon">Deletar &nbsp;</span>
+            <FiTrash2 className="danger-icon" />
+          </MenuItem>
+        )}
+      </Menu>
+    </>
+  );
+};
+
+export default memo(OptionsMenu);
