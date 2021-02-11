46e62ba3c170c355dafea5225fe5381fee99f95c
yuri014 <yuriflores2012@gmail.com>
2021-02-10 23:35:43 -0300

    creating post page

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import { gql, useQuery } from '@apollo/client';
+
+import Header from '../../components/Header';
+import MobileFooter from '../../components/MobileFooter';
+import { initializeApollo } from '../../graphql/apollo/config';
+import { GET_POST } from '../../graphql/queries/post';
+import { IPost, PostProps } from '../../interfaces/Post';
+import AudioPost from '../../components/Post/AudioPost';
+import ImagePost from '../../components/Post/ImagePost';
+import PostPageContainer from '../../styles/pages/post/styles';
+
+interface PostQuery {
+  getPost: IPost;
+}
+
+const GET_IS_LIKED = gql`
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      isLiked
+    }
+  }
+`;
+
+const Post: React.FC<PostProps> = ({ post }) => {
+  const [postData, setPostData] = useState<IPost>();
+  const { data } = useQuery<PostQuery>(GET_IS_LIKED, {
+    variables: { id: post._id },
+  });
+
+  return (
+    <PostPageContainer>
+      <Header />
+      <main>
+        <div className="container">
+          {post.isAudio ? <AudioPost post={post} /> : <ImagePost post={post} />}
+        </div>
+      </main>
+      <MobileFooter />
+    </PostPageContainer>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { id } = context.params;
+
+  const client = initializeApollo();
+
+  const post = await client.query({
+    query: GET_POST,
+    variables: { id },
+    errorPolicy: 'ignore',
+  });
+
+  if (!post.data.getPost) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post: { ...post.data.getPost, _id: id },
+    },
+  };
+};
+
+export default Post;
