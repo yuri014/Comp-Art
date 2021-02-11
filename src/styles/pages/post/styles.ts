4ef1f8d23b46d707200ce5b983681767fbfcff80
yuri014 <yuriflores2012@gmail.com>
2021-02-10 23:35:07 -0300

    post page style

---
+++ src/styles/pages/post/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const PostPageContainer = styled.div`
+  main {
+    .container {
+      width: 100%;
+      height: 100vh;
+      padding: 0 2rem;
+      display: flex;
+      align-items: center;
+      max-width: 768px;
+    }
+  }
+`;
+
+export default PostPageContainer;
