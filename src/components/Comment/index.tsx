d58e39bcdc9dc0d1ff1f2d7a6f72fb665fbde2da
yuri014 <yuriflores2012@gmail.com>
2021-02-13 18:47:03 -0300

    add props

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,23 +3,29 @@ import React from 'react';
 import { FaRegHeart } from 'react-icons/fa';
 import CommentContainer from './styles';
 
-const Comment: React.FC = () => (
+export interface CommentProps {
+  avatar: string;
+  owner: {
+    name: string;
+    username: string;
+  };
+  text: string;
+}
+
+const Comment: React.FC<CommentProps> = ({ avatar, owner, text }) => (
   <CommentContainer>
     <div className="author-image">
-      <img src="/profile.jpg" alt="Author" />
+      <img src={avatar || '/profile.jpg'} alt={`${owner.name}`} />
     </div>
     <div className="comment">
       <div className="comment-body">
         <Link href="/profile">
           <a className="author-info">
-            <p className="name">Teste</p>
-            <p className="username">@teste10</p>
+            <p className="name">{owner.name}</p>
+            <p className="username">@{owner.username}</p>
           </a>
         </Link>
-        <p className="comment-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-          aliquam!
-        </p>
+        <p className="comment-text">{text}</p>
       </div>
       <div className="comment-interations">
         <FaRegHeart /> <p>Curtir</p>
