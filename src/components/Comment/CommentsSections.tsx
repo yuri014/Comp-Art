e5c0388affab3c8f7f60317abcfd9158f6f3eab6
yuri014 <yuriflores2012@gmail.com>
2021-02-13 19:07:34 -0300

    adding auth provider

--- src/components/Comment/CommentsSections.tsx
+++ src/components/Comment/CommentsSections.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
 import { IconButton, TextField, ThemeProvider } from '@material-ui/core';
 
 import Comment, { CommentProps } from '.';
 import mainTheme from '../../styles/themes/MainTheme';
 import { CommentsSectionContainer } from './styles';
+import { AuthContext } from '../../context/auth';
 
 const CommentsSections: React.FC = () => {
+  const auth = useContext(AuthContext);
   const [newComment, setNewComment] = useState<Array<CommentProps>>([]);
 
   return (
@@ -21,38 +23,40 @@ const CommentsSections: React.FC = () => {
             />
           ))}
       </div>
-      <ThemeProvider theme={mainTheme}>
-        <footer>
-          <img src="/profile.jpg" alt="Profile name" />
-          <TextField
-            id="send-comment"
-            label="Enviar um comentário"
-            fullWidth
-            multiline
-          />
-          <IconButton
-            onClick={() =>
-              setNewComment([
-                ...newComment,
-                {
-                  avatar: '/profile.jpg',
-                  owner: {
-                    name: 'Teste',
-                    username: 'teste01',
+      {auth.user && (
+        <ThemeProvider theme={mainTheme}>
+          <footer>
+            <img src="/profile.jpg" alt="Profile name" />
+            <TextField
+              id="send-comment"
+              label="Enviar um comentário"
+              fullWidth
+              multiline
+            />
+            <IconButton
+              onClick={() =>
+                setNewComment([
+                  ...newComment,
+                  {
+                    avatar: '/profile.jpg',
+                    owner: {
+                      name: 'Teste',
+                      username: 'teste01',
+                    },
+                    text: 'exemplo 123',
                   },
-                  text: 'exemplo 123',
-                },
-              ])
-            }
-            aria-label="enviar comentário"
-            color="primary"
-          >
-            <div className="send-button">
-              <AiOutlineSend />
-            </div>
-          </IconButton>
-        </footer>
-      </ThemeProvider>
+                ])
+              }
+              aria-label="enviar comentário"
+              color="primary"
+            >
+              <div className="send-button">
+                <AiOutlineSend />
+              </div>
+            </IconButton>
+          </footer>
+        </ThemeProvider>
+      )}
     </CommentsSectionContainer>
   );
 };
