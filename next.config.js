01692ad1a142f14e4a624881caa4719407cadf83
yuri014 <yuriflores2012@gmail.com>
2021-02-14 13:05:28 -0300

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 const withImages = require('next-images');
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withImages({
+const imagesPlugin = withImages({
   esModule: true,
 });
 
-module.exports = {
+const pwaPlugin = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    runtimeCaching,
+  },
+});
+
+module.exports = withPlugins([imagesPlugin, pwaPlugin, {
   images: {
     domains: ['localhost'],
   },
-}
+}]);
