25a54b6279e07d98aebedd3578320efd4ade099d
yuri014 <yuriflores2012@gmail.com>
2021-02-20 20:39:15 -0300

    merge cache get likes

--- src/graphql/apollo/config.ts
+++ src/graphql/apollo/config.ts
@@ -67,6 +67,16 @@ function createApolloClient() {
                 return merged;
               },
             },
+            getLikes: {
+              keyArgs: false,
+              merge(existing, incoming, { args: { offset = 0 } }) {
+                const merged = existing ? existing.slice(0) : [];
+                for (let i = 0; i < incoming.length; ++i) {
+                  merged[offset + i] = incoming[i];
+                }
+                return merged;
+              },
+            },
           },
         },
       },
