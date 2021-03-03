da345e82de07c8be9662ca24a5a62ee775adb4d9
yuri014 <yuriflores2012@gmail.com>
2021-03-02 21:07:21 -0300

    code spliting audio post

---
+++ src/components/Post/AudioPost/Links.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface LinksProps {
+  username: string;
+  description: string;
+  name: string;
+  id: string;
+}
+
+const Links = React.memo<LinksProps>(({ username, description, name, id }) => (
+  <div>
+    <Link href={`/post/${id}`}>
+      <a>
+        <p className="music-name">{description}</p>
+      </a>
+    </Link>
+    <Link href={`/profile/${username}`}>
+      <a>
+        <p className="artist-name">{name}</p>
+      </a>
+    </Link>
+  </div>
+));
+
+export default Links;
