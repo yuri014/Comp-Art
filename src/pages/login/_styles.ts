8d7cc6caad0dc130707bd6a529e6d1e245e44c30
yuri014 <yuriflores2012@gmail.com>
2021-03-30 19:53:57 -0300

    fix height

--- src/pages/login/_styles.ts
+++ src/pages/login/_styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 const LoginContainer = styled.div`
-  height: 100vh;
-  padding: 4rem 0;
+  padding: 2rem 0;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
