c1d179fe0eae5de8d1a5e5d35c878a87ee1f8075
yuri014 <yuriflores2012@gmail.com>
2021-03-31 21:12:38 -0300

    choose profile component

---
+++ src/components/Splitter/ChooseProfile.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface ChooseProfileProps {
+  isArtist: boolean;
+  setIsArtist: (_args: boolean) => void;
+}
+
+const ChooseProfile: React.FC<ChooseProfileProps> = ({
+  isArtist,
+  setIsArtist,
+}) => (
+  <div className="choose-profile">
+    <button
+      onClick={() => setIsArtist(true)}
+      type="button"
+      className={isArtist && 'active'}
+    >
+      <img src="/assets/avatar-artist.svg" alt="Representação de um artista" />
+      <p>Cadastrar como Artista</p>
+    </button>
+    <button
+      type="button"
+      onClick={() => setIsArtist(false)}
+      className={!isArtist && 'active'}
+    >
+      <img src="/assets/avatar-user.svg" alt="Representação de um fã" />
+      <p>Cadastrar como Fã</p>
+    </button>
+  </div>
+);
+
+export default ChooseProfile;
