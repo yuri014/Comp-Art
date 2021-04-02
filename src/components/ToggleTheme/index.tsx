ae5f26d6d631804deb055fe0304aa43356f37301
yuri014 <yuriflores2012@gmail.com>
2021-04-02 20:16:38 -0300

    changing function to utils toggle theme

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -5,19 +5,10 @@ import ThemeContext from '../../context/theme';
 import LightIcon from '../../assets/light-toggle.svg';
 import DarkIcon from '../../assets/dark-toggle.svg';
 import ToggleThemeContainer from './_styles';
+import toggleTheme from '../../utils/toggleTheme';
 
 const ToggleThemeButton: React.FC = () => {
   const { theme, setTheme } = useContext(ThemeContext);
-
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      window.localStorage.setItem('theme', 'dark');
-      setTheme('dark');
-    } else {
-      window.localStorage.setItem('theme', 'light');
-      setTheme('light');
-    }
-  };
 
   useEffect(() => {
     setTheme(window.localStorage.getItem('theme'));
@@ -27,7 +18,7 @@ const ToggleThemeButton: React.FC = () => {
     <ToggleThemeContainer>
       <IconButton
         aria-label={`mudar para tema ${theme === 'light' ? 'claro' : 'escuro'}`}
-        onClick={toggleTheme}
+        onClick={() => toggleTheme(theme, setTheme)}
       >
         {theme === 'light' ? <LightIcon /> : <DarkIcon />}
       </IconButton>
