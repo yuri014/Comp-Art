a5ea607d57af4a600e9fe49a38b90d17d099a53c
yuri014 <yuriflores2012@gmail.com>
2021-04-04 20:15:13 -0300

    adding two main themes

---
+++ src/styles/themes/MainLightTheme.ts
@@ -0,0 +1,38 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const mainLightTheme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#1cc5b7',
+    },
+    secondary: {
+      main: '#cbcaeb',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: ['-apple-system', 'BlinkMacSystemFont', '"Jura"'].join(','),
+    fontSize: 22,
+  },
+  overrides: {
+    MuiCard: {
+      root: {
+        background: '#08162f',
+      },
+    },
+    MuiMenu: {
+      paper: {
+        background: '#f5f5f5',
+      },
+    },
+    MuiMenuItem: {
+      root: {
+        padding: '0',
+      },
+    },
+  },
+});
+
+export default mainLightTheme;
