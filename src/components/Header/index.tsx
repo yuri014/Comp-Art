4aa9c41a5dc72b7fde8010d8905b5500cde4908e
yuri014 <yuriflores2012@gmail.com>
2021-04-06 20:39:08 -0300

    fix header without container

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -36,97 +36,95 @@ const Header: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
 
   return (
     <HeaderContainer>
-      <div className="container">
-        <Link href="/home">
-          <a>
-            <p>CompArt</p>
+      <Link href="/home">
+        <a>
+          <p>CompArt</p>
+        </a>
+      </Link>
+      <div className="search-input">
+        <input type="text" title="Buscar..." placeholder="Buscar..." />
+        <div className="search-button">
+          <IconButton type="button" aria-label="Buscar">
+            <FaSearch />
+          </IconButton>
+        </div>
+      </div>
+      <div className="header-icons">
+        <Link href="/market">
+          <a title="Ir para o marketplace">
+            <FaShoppingCart />
           </a>
         </Link>
-        <div className="search-input">
-          <input type="text" title="Buscar..." placeholder="Buscar..." />
-          <div className="search-button">
-            <IconButton type="button" aria-label="Buscar">
-              <FaSearch />
-            </IconButton>
-          </div>
-        </div>
-        <div className="header-icons">
-          <Link href="/market">
-            <a title="Ir para o marketplace">
-              <FaShoppingCart />
-            </a>
-          </Link>
-          <Link href="/market">
-            <a title="Ir para o marketplace">
-              <FaBell />
-            </a>
-          </Link>
-          <Link href="/market">
-            <a title="Ir para os salvos">
-              <FaBookmark />
-            </a>
-          </Link>
-          <ThemeProvider
-            theme={theme === 'light' ? mainLightTheme : mainDarkTheme}
+        <Link href="/market">
+          <a title="Ir para o marketplace">
+            <FaBell />
+          </a>
+        </Link>
+        <Link href="/market">
+          <a title="Ir para os salvos">
+            <FaBookmark />
+          </a>
+        </Link>
+        <ThemeProvider
+          theme={theme === 'light' ? mainLightTheme : mainDarkTheme}
+        >
+          <IconButton
+            aria-controls="menu-header"
+            aria-haspopup="true"
+            aria-label="Abrir menu de configurações"
+            onClick={handleClick}
+            color="secondary"
           >
-            <IconButton
-              aria-controls="menu-header"
-              aria-haspopup="true"
-              aria-label="Abrir menu de configurações"
-              onClick={handleClick}
-              color="secondary"
-            >
-              <img
-                src={process.env.NEXT_PUBLIC_API_HOST + getLoggedProfile.avatar}
-                alt="Foto do perfil"
-              />
-            </IconButton>
-            <Menu
-              id="menu-header"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem>
-                <Link href={`/profile/${getLoggedProfile.owner}`}>
-                  <MenuListIcon as="a">
-                    <FaUserAlt />
-                    <p>Meu Perfil</p>
-                  </MenuListIcon>
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <MenuListIcon onClick={() => toggleTheme(theme, setTheme)}>
-                  <FaMoon />
-                  <p>Modo {theme === 'light' ? 'Escuro' : 'Claro'}</p>
+            <img
+              src={process.env.NEXT_PUBLIC_API_HOST + getLoggedProfile.avatar}
+              alt="Foto do perfil"
+            />
+          </IconButton>
+          <Menu
+            id="menu-header"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem>
+              <Link href={`/profile/${getLoggedProfile.owner}`}>
+                <MenuListIcon as="a">
+                  <FaUserAlt />
+                  <p>Meu Perfil</p>
                 </MenuListIcon>
-              </MenuItem>
-              <MenuItem>
-                <Link href="/changelog">
-                  <MenuListIcon as="a">
-                    <FaExchangeAlt />
-                    <p>Changelog</p>
-                  </MenuListIcon>
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <Link href="/config">
-                  <MenuListIcon as="a">
-                    <FaCog />
-                    <p>Configurações</p>
-                  </MenuListIcon>
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={() => auth.logout()}>
-                <MenuListIcon>
-                  <FaSignOutAlt />
-                  <p>Sair</p>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <MenuListIcon onClick={() => toggleTheme(theme, setTheme)}>
+                <FaMoon />
+                <p>Modo {theme === 'light' ? 'Escuro' : 'Claro'}</p>
+              </MenuListIcon>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/changelog">
+                <MenuListIcon as="a">
+                  <FaExchangeAlt />
+                  <p>Changelog</p>
                 </MenuListIcon>
-              </MenuItem>
-            </Menu>
-          </ThemeProvider>
-        </div>
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link href="/config">
+                <MenuListIcon as="a">
+                  <FaCog />
+                  <p>Configurações</p>
+                </MenuListIcon>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={() => auth.logout()}>
+              <MenuListIcon>
+                <FaSignOutAlt />
+                <p>Sair</p>
+              </MenuListIcon>
+            </MenuItem>
+          </Menu>
+        </ThemeProvider>
       </div>
     </HeaderContainer>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,73 +19,70 @@ export const HeaderContainer = styled.header`
   color: ${({ theme }) => theme.colors.lightContrast};
   position: fixed;
   width: 100%;
-  padding: 1rem 0;
+  padding: 1rem 4rem;
   font-size: 1.8rem;
   z-index: 2;
 
-  .container {
-    display: grid;
-    grid-template-columns: 20% 40% 20%;
+  display: grid;
+  grid-template-columns: 20% 40% 20%;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    font: 400 2.8rem ${({ theme }) => theme.fonts.display};
+    color: ${({ theme }) => theme.colors.themeColor};
+  }
+
+  .search-input {
+    position: relative;
+    display: flex;
+    align-items: center;
+
+    input {
+      padding: 1.4rem 2rem;
+      background-color: ${({ theme }) => theme.colors.backgroundColor};
+      border: none;
+      border-radius: 5px;
+      width: 100%;
+      height: 100%;
+      color: ${({ theme }) => theme.colors.themeColor};
+      box-shadow: ${({ theme }) => theme.colors.secondaryShadow};
+
+      &:focus {
+        padding: calc(1.4rem - 1px) 2rem;
+        border: 1px solid #949494;
+        outline: none;
+      }
+    }
+
+    .search-button {
+      position: absolute;
+      left: calc(100% - 4.6rem);
+      button {
+        color: ${({ theme }) => theme.colors.gray};
+        font-size: 1.6rem;
+      }
+    }
+  }
+
+  .header-icons {
+    display: flex;
     align-items: center;
     justify-content: space-between;
 
-    p {
-      font: 400 2.8rem ${({ theme }) => theme.fonts.display};
-      color: ${({ theme }) => theme.colors.themeColor};
+    a {
+      color: ${({ theme }) => theme.colors.gray};
     }
 
-    .search-input {
-      position: relative;
-      display: flex;
-      align-items: center;
+    button {
+      padding: 0.2rem;
 
-      input {
-        padding: 1.4rem 2rem;
-        background-color: ${({ theme }) => theme.colors.backgroundColor};
-        border: none;
-        border-radius: 5px;
-        width: 100%;
-        height: 100%;
-        color: ${({ theme }) => theme.colors.themeColor};
-        box-shadow: ${({ theme }) => theme.colors.secondaryShadow};
-
-        &:focus {
-          padding: calc(1.4rem - 1px) 2rem;
-          border: 1px solid #949494;
-          outline: none;
-        }
-      }
-
-      .search-button {
-        position: absolute;
-        left: calc(100% - 4.6rem);
-        button {
-          color: ${({ theme }) => theme.colors.gray};
-          font-size: 1.6rem;
-        }
-      }
-    }
-
-    .header-icons {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding-left: 6rem;
-
-      a {
-        color: ${({ theme }) => theme.colors.gray};
-      }
-
-      button {
-        padding: 0.2rem;
-
-        img {
-          width: 3rem;
-          height: 3rem;
-          border-radius: 50%;
-          cursor: pointer;
-          object-fit: fill;
-        }
+      img {
+        width: 3rem;
+        height: 3rem;
+        border-radius: 50%;
+        cursor: pointer;
+        object-fit: fill;
       }
     }
   }
