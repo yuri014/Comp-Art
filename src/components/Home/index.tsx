baeeffb9c8510d88ab8cab8d7dbe5aadf55a3a2e
yuri014 <yuriflores2012@gmail.com>
2021-04-08 19:28:28 -0300

    code splitting timeline

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,67 +1,30 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
 
 import HomeProfile from '../HomeProfile';
-import Post from '../Post';
-import LoadingPost from '../Post/LoadingPost';
 import QuestsProgress from '../QuestsProgress';
-import { IGetPosts } from '../../interfaces/Post';
-import { GET_POSTS } from '../../graphql/queries/post';
 import { IProfile } from '../../interfaces/Profile';
-import useInfiniteScroll from '../../hooks/infiniteScroll';
 import SuggestedProfiles from '../SuggestedProfiles';
+import Timeline from '../Splitter/Timeline';
 
 interface HomeProps {
   getLoggedProfile: IProfile;
 }
 
-const Home: React.FC<HomeProps> = ({ getLoggedProfile }) => {
-  const { data, loading, error, fetchMore } = useQuery<IGetPosts>(GET_POSTS, {
-    variables: { offset: 0 },
-  });
-
-  const lastPostRef = useInfiniteScroll(
-    data,
-    () =>
-      !!data.getPosts &&
-      fetchMore({
-        variables: { offset: data.getPosts.length },
-      }).then(newPosts => newPosts.data.getPosts.length < 3),
-  );
-
-  return (
-    <div className="home-desktop-content">
-      <aside>
-        <HomeProfile getLoggedProfile={getLoggedProfile} />
-      </aside>
-      <div className="timeline">
-        {loading || error ? (
-          <LoadingPost loading={loading} />
-        ) : (
-          data.getPosts.map((post, index) => {
-            if (data.getPosts.length === index + 1) {
-              return (
-                <div key={`${post.artist}_${post.createdAt}`} ref={lastPostRef}>
-                  <Post post={post} />
-                </div>
-              );
-            }
-            return (
-              <div key={`${post.artist}_${post.createdAt}`}>
-                <Post post={post} />
-              </div>
-            );
-          })
-        )}
-      </div>
-      <aside>
-        <SuggestedProfiles />
-        <br />
-        <br />
-        <QuestsProgress />
-      </aside>
+const Home: React.FC<HomeProps> = ({ getLoggedProfile }) => (
+  <div className="home-desktop-content">
+    <aside>
+      <HomeProfile getLoggedProfile={getLoggedProfile} />
+    </aside>
+    <div className="timeline">
+      <Timeline />
     </div>
-  );
-};
+    <aside>
+      <SuggestedProfiles />
+      <br />
+      <br />
+      <QuestsProgress />
+    </aside>
+  </div>
+);
 
 export default React.memo(Home);
---
+++ src/components/Splitter/Timeline.tsx
@@ -0,0 +1,47 @@
+import { useQuery } from '@apollo/client';
+import React from 'react';
+
+import Post from '../Post';
+import LoadingPost from '../Post/LoadingPost';
+import { GET_POSTS } from '../../graphql/queries/post';
+import { IGetPosts } from '../../interfaces/Post';
+import useInfiniteScroll from '../../hooks/infiniteScroll';
+
+const Timeline: React.FC = () => {
+  const { data, loading, error, fetchMore } = useQuery<IGetPosts>(GET_POSTS, {
+    variables: { offset: 0 },
+  });
+
+  const lastPostRef = useInfiniteScroll(
+    data,
+    () =>
+      !!data.getPosts &&
+      fetchMore({
+        variables: { offset: data.getPosts.length },
+      }).then(newPosts => newPosts.data.getPosts.length < 3),
+  );
+  return (
+    <>
+      {loading || error || data.getPosts.length === 0 ? (
+        <LoadingPost loading={loading} />
+      ) : (
+        data.getPosts.map((post, index) => {
+          if (data.getPosts.length === index + 1) {
+            return (
+              <div key={`${post.artist}_${post.createdAt}`} ref={lastPostRef}>
+                <Post post={post} />
+              </div>
+            );
+          }
+          return (
+            <div key={`${post.artist}_${post.createdAt}`}>
+              <Post post={post} />
+            </div>
+          );
+        })
+      )}
+    </>
+  );
+};
+
+export default Timeline;
