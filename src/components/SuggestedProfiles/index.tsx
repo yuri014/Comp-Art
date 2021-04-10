2d093f781005b1605107b26f07a470a251bcae61
yuri014 <yuriflores2012@gmail.com>
2021-04-10 15:53:09 -0300

    adding backend data to suggested profile component

--- src/components/SuggestedProfiles/index.tsx
+++ src/components/SuggestedProfiles/index.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import SuggestedProfilesContainer from './styles';
+import LoadingSuggestedProfile from './loading';
+import { IProfile } from '../../interfaces/Profile';
 
-const SuggestedProfiles: React.FC = () => (
-  <SuggestedProfilesContainer>
-    <h4>Sugestões para seguir</h4>
-    <div className="suggested-profile-container">
-      <div className="suggested-profile">
-        <img
-          src="https://wallpaperaccess.com/full/2213424.jpg"
-          alt="Nome do usuário"
-        />
+const GET_SUGGESTED_PROFILES = gql`
+  query GetSuggestedProfiles {
+    getSuggestedProfiles {
+      avatar
+      name
+      owner
+    }
+  }
+`;
 
-        <div className="suggested-profile-info">
-          <p className="limited-text">Example</p>
-          <p className="limited-text">@example01</p>
-        </div>
-      </div>
-      <button type="button">SEGUIR</button>
-    </div>
-  </SuggestedProfilesContainer>
-);
+interface ISuggestedProfile {
+  getSuggestedProfiles: Array<IProfile>;
+}
+
+const SuggestedProfiles: React.FC = () => {
+  const { data, loading } = useQuery<ISuggestedProfile>(GET_SUGGESTED_PROFILES);
+
+  return (
+    <SuggestedProfilesContainer>
+      <h4>Sugestões para seguir</h4>
+      {loading ? (
+        <LoadingSuggestedProfile />
+      ) : (
+        <>
+          {data.getSuggestedProfiles.map(profile => (
+            <div key={profile.owner} className="suggested-profile-container">
+              <div className="suggested-profile">
+                <img
+                  src={process.env.NEXT_PUBLIC_API_HOST + profile.avatar}
+                  alt={profile.name}
+                />
+
+                <div className="suggested-profile-info">
+                  <p className="limited-text">{profile.name}</p>
+                  <p className="limited-text">@{profile.owner}</p>
+                </div>
+              </div>
+              <button type="button">SEGUIR</button>
+            </div>
+          ))}
+        </>
+      )}
+    </SuggestedProfilesContainer>
+  );
+};
 
 export default SuggestedProfiles;
