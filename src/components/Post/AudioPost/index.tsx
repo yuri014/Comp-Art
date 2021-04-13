f2a98241311cb4754daada5ed070acfb823a4975
yuri014 <yuriflores2012@gmail.com>
2021-04-13 00:26:30 -0300

    removing older version components

--- src/components/Post/AudioPost/index.tsx
+++ src/components/Post/AudioPost/index.tsx
@@ -1,62 +1,26 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
-import { FaBackward, FaForward, FaPause, FaPlay } from 'react-icons/fa';
+import {
+  FaBackward,
+  FaForward,
+  FaPause,
+  FaPlay,
+  FaVolumeUp,
+} from 'react-icons/fa';
 import { Slider, ThemeProvider } from '@material-ui/core';
-import dynamic from 'next/dynamic';
 
+import { FiRepeat } from 'react-icons/fi';
 import AudioPostContainer from './audioPostStyles';
 import mainTheme from '../../../styles/themes/MainTheme';
 import { PostProps } from '../../../interfaces/Post';
-import useDeletePost from '../../../hooks/posts';
-import LevelContext from '../../../context/level';
-import Interactions from './Interactions';
 import Links from './Links';
-import { GET_LIKES } from '../../../graphql/mutations/post';
-
-const OptionsMenu = dynamic(() => import('../OptionsMenu'));
-const ModalProfile = dynamic(() => import('../../ModalProfile'));
 
 const AudioPost: React.FC<PostProps> = ({ post }) => {
   const audioRef = useRef<null | HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [audioDuration, setAudioDuration] = useState('0:00');
   const [currentTime, setCurrentTime] = useState('');
-  const [likesCount, setLikesCount] = useState(0);
-  const [isLiked, setIsLiked] = useState(false);
   const [slider, setSlider] = useState(0);
-  const [isDeleted, setIsDeleted] = useState(false);
-  const [modalShow, setModalShow] = useState(false);
-
-  const levelContext = useContext(LevelContext);
-
-  useEffect(() => {
-    setLikesCount(post.likesCount);
-    setIsLiked(post.isLiked);
-  }, [post.isLiked, post.likesCount]);
-
-  const [deletePost, dislikePost, likePost] = useDeletePost(
-    post._id,
-    () => {
-      setIsLiked(false);
-      setLikesCount(likesCount - 1);
-
-      if (levelContext) {
-        levelContext.updateLevel();
-      }
-    },
-    () => {
-      setIsLiked(true);
-      setLikesCount(likesCount + 1);
-      if (levelContext) {
-        levelContext.updateLevel();
-      }
-    },
-  );
-
-  const handleDeletePost = () => {
-    deletePost();
-    setIsDeleted(true);
-  };
 
   const getTime = (time: number) => {
     const minutes = Math.floor(time / 60);
@@ -104,121 +68,78 @@ const AudioPost: React.FC<PostProps> = ({ post }) => {
   };
 
   return (
-    <>
-      {!isDeleted && (
-        <AudioPostContainer>
-          <ThemeProvider theme={mainTheme}>
-            <div className="audio-card">
-              <div className="audio-card-content">
-                <div className="audio-card-info">
-                  <Links
-                    username={post.artist.owner}
-                    description={post.description}
-                    name={post.artist.name}
-                    id={post._id}
-                  />
-                  <div className="options">
-                    <OptionsMenu
-                      deletePost={handleDeletePost}
-                      id={post._id}
-                      username={post.artist.owner}
-                    />
-                  </div>
-                </div>
-                <div className="audio-buttons">
-                  <IconButton
-                    onClick={() => {
-                      audioRef.current.currentTime -= 10;
-                    }}
-                    aria-label="previous"
-                  >
-                    <FaBackward />
-                  </IconButton>
-                  <IconButton
-                    onClick={() => handlePlaying()}
-                    aria-label="play/pause"
-                  >
-                    {!isPlaying ? <FaPlay /> : <FaPause />}
-                  </IconButton>
-                  <IconButton
-                    onClick={() => {
-                      audioRef.current.currentTime += 10;
-                    }}
-                    aria-label="next"
-                  >
-                    <FaForward />
-                  </IconButton>
-                </div>
-                <div className="progress">
-                  <div className="duration">
-                    <p>{currentTime || '0:00'}</p>
-                    <p>{audioDuration || '0:00'}</p>
-                  </div>
-                  <Slider
-                    value={slider}
-                    min={0}
-                    max={audioRef.current && audioRef.current.duration}
-                    onChange={handleScroll}
-                    aria-label="input-slider"
-                    step={0.01}
-                  />
-                </div>
-              </div>
-              <Interactions
-                avatar={post.artist.avatar}
-                dislikePost={dislikePost}
-                isLiked={isLiked}
-                likePost={likePost}
+    <AudioPostContainer>
+      <ThemeProvider theme={mainTheme}>
+        <div className="audio-card">
+          <div className="audio-card-content">
+            <div className="audio-card-info">
+              <Links
+                username={post.artist.owner}
+                description={post.description}
                 name={post.artist.name}
                 id={post._id}
               />
             </div>
-            <div className="post-counts">
-              <button
-                onClick={() => setModalShow(true)}
-                type="button"
-                aria-label="Abrir modal de likes"
-              >
-                <div className="likes-images">
-                  {post.likes &&
-                    post.likes.map(({ profile }) => (
-                      <img
-                        key={profile.owner}
-                        src={process.env.NEXT_PUBLIC_API_HOST + profile.avatar}
-                        alt={profile.owner}
-                        title={profile.owner}
-                      />
-                    ))}
-                </div>
-                {likesCount > 0 && (
-                  <>
-                    {likesCount} {likesCount > 1 ? 'curtidas' : 'curtida'}
-                  </>
-                )}
-              </button>
+            <div className="audio-buttons">
+              <IconButton aria-label="volume">
+                <FaVolumeUp />
+              </IconButton>
+              <div className="primary-audio-button">
+                <IconButton
+                  onClick={() => {
+                    audioRef.current.currentTime -= 10;
+                  }}
+                  aria-label="voltar 10 segundos"
+                >
+                  <FaBackward />
+                </IconButton>
+                <IconButton
+                  onClick={() => handlePlaying()}
+                  aria-label="play/pause"
+                >
+                  {!isPlaying ? <FaPlay /> : <FaPause />}
+                </IconButton>
+                <IconButton
+                  onClick={() => {
+                    audioRef.current.currentTime += 10;
+                  }}
+                  aria-label="avançar 10 segundos"
+                >
+                  <FaForward />
+                </IconButton>
+              </div>
+              <IconButton aria-label="repetir">
+                <FiRepeat />
+              </IconButton>
             </div>
-            <audio
-              style={{ display: 'none' }}
-              ref={audioRef}
-              src={process.env.NEXT_PUBLIC_API_HOST + post.body}
-              onLoadedMetadata={() => {
-                const { minutes, seconds } = getTime(audioRef.current.duration);
-                setAudioDuration(`${minutes}:${seconds}`);
-              }}
-              controls
-            />
-          </ThemeProvider>
-        </AudioPostContainer>
-      )}
-      {modalShow && (
-        <ModalProfile
-          onHide={() => setModalShow(false)}
-          queryResult="getLikes"
-          query={GET_LIKES}
-          id={post._id}
+            <div className="progress">
+              <div className="duration">
+                <p>{currentTime || '0:00'}</p>
+                <p>{audioDuration || '0:00'}</p>
+              </div>
+              <Slider
+                value={slider}
+                min={0}
+                max={audioRef.current && audioRef.current.duration}
+                onChange={handleScroll}
+                aria-label="input-slider"
+                step={0.01}
+              />
+            </div>
+          </div>
+        </div>
+        <audio
+          style={{ display: 'none' }}
+          ref={audioRef}
+          src={process.env.NEXT_PUBLIC_API_HOST + post.body}
+          onLoadedMetadata={() => {
+            const { minutes, seconds } = getTime(audioRef.current.duration);
+            setAudioDuration(`${minutes}:${seconds}`);
+          }}
+          controls
         />
-      )}
-    </>
+      </ThemeProvider>
+    </AudioPostContainer>
   );
 };
 
