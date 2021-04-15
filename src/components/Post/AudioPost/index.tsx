3873e70539233332573581484fdc50f9c437cf57
yuri014 <yuriflores2012@gmail.com>
2021-04-14 23:06:23 -0300

    refactoring to prevent re-render

--- src/components/Post/AudioPost/index.tsx
+++ src/components/Post/AudioPost/index.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import {
   FaBackward,
@@ -13,7 +7,7 @@ import {
   FaPlay,
   FaVolumeUp,
 } from 'react-icons/fa';
-import { Slider, ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core';
 
 import { FiRepeat } from 'react-icons/fi';
 import AudioPostContainer from './audioPostStyles';
@@ -22,64 +16,31 @@ import Links from './Links';
 import ThemeContext from '../../../context/theme';
 import mainDarkTheme from '../../../styles/themes/MainDarkTheme';
 import mainLightTheme from '../../../styles/themes/MainLightTheme';
+import AudioSlider from './Slider';
 
 const AudioPost: React.FC<PostProps> = ({ post }) => {
-  const audioRef = useRef<null | HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [audioDuration, setAudioDuration] = useState('0:00');
-  const [currentTime, setCurrentTime] = useState('');
-  const [slider, setSlider] = useState(0);
+  const [audio, setAudio] = useState<HTMLAudioElement>();
   const { theme } = useContext(ThemeContext);
-
-  const getTime = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time - minutes * 60);
-    return { minutes, seconds };
-  };
-
-  const setTime = useCallback(() => {
-    const time = audioRef.current.currentTime;
-    const { minutes, seconds } = getTime(time);
-    setCurrentTime(`${minutes}:${Math.floor(seconds)}`);
-    return time;
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const current = setTime();
-      setSlider(audioRef.current.currentTime);
-      if (current === audioRef.current.duration) {
-        setIsPlaying(false);
-      }
-    }, 20);
-
-    if (!isPlaying) {
-      clearInterval(interval);
-    }
-
-    return () => clearInterval(interval);
-  }, [isPlaying, setTime]);
 
   const handlePlaying = () => {
     if (!isPlaying) {
-      audioRef.current.play();
+      audio.play();
       return setIsPlaying(true);
     }
-    audioRef.current.pause();
+    audio.pause();
     return setIsPlaying(false);
   };
 
   useEffect(() => {
-    if (audioRef.current.duration) {
-      setTime();
-    }
-  }, [setTime]);
+    const newAudio = new Audio(process.env.NEXT_PUBLIC_API_HOST + post.body);
+    setAudio(newAudio);
 
-  const handleScroll = (_, newValue: number | number[]) => {
-    audioRef.current.currentTime = newValue as number;
-    setSlider(newValue as number);
-    setTime();
-  };
+    newAudio.addEventListener('ended', () => setIsPlaying(false));
+
+    return () =>
+      newAudio.removeEventListener('ended', () => setIsPlaying(false));
+  }, [post.body]);
 
   return (
     <AudioPostContainer>
@@ -101,7 +62,7 @@ const AudioPost: React.FC<PostProps> = ({ post }) => {
               <div className="primary-audio-button">
                 <IconButton
                   onClick={() => {
-                    audioRef.current.currentTime -= 10;
+                    audio.currentTime -= 10;
                   }}
                   aria-label="voltar 10 segundos"
                 >
@@ -115,7 +76,7 @@ const AudioPost: React.FC<PostProps> = ({ post }) => {
                 </IconButton>
                 <IconButton
                   onClick={() => {
-                    audioRef.current.currentTime += 10;
+                    audio.currentTime += 10;
                   }}
                   aria-label="avançar 10 segundos"
                 >
@@ -126,37 +87,18 @@ const AudioPost: React.FC<PostProps> = ({ post }) => {
                 <FiRepeat className="secondary-button" />
               </IconButton>
             </div>
-            <div className="progress">
-              <Slider
-                value={slider}
-                min={0}
-                max={audioRef.current && audioRef.current.duration}
-                onChange={handleScroll}
-                aria-label="input-slider"
-                step={0.01}
+            {audio && (
+              <AudioSlider
+                audio={audio}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
               />
-              <div className="duration">
-                <p>{currentTime || '0:00'}</p>
-                <p>{audioDuration || '0:00'}</p>
-              </div>
-            </div>
+            )}
           </div>
         </div>
-        {post.body && (
-          <audio
-            style={{ display: 'none' }}
-            ref={audioRef}
-            src={process.env.NEXT_PUBLIC_API_HOST + post.body}
-            onLoadedMetadata={() => {
-              const { minutes, seconds } = getTime(audioRef.current.duration);
-              setAudioDuration(`${minutes}:${seconds}`);
-            }}
-            controls
-          />
-        )}
       </ThemeProvider>
     </AudioPostContainer>
   );
 };
 
-export default React.memo(AudioPost);
+export default AudioPost;
