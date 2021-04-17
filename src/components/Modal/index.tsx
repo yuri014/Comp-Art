a5ee5f25d1c117749e100025155a401136d857c0
yuri014 <yuriflores2012@gmail.com>
2021-04-17 17:06:47 -0300

    refactoring modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { FaTimesCircle } from 'react-icons/fa';
+import { CgClose } from 'react-icons/cg';
 import ModalContainer from './styles';
 
 interface ModalProps {
   show: boolean;
   onHide: () => void;
   title: string;
+  text: string;
 }
 
 const Modal: React.FC<ModalProps> = props => (
-  <ModalContainer>
-    <div className={`modal-block-${props.show}`}>
+  <ModalContainer className={`modal-block-${props.show}`}>
+    <div className="modal">
+      <button type="button" onClick={props.onHide} className="close-modal">
+        <CgClose />
+      </button>
       <div className="modal-content">
         <div className="modal-title">
           <p>{props.title}</p>
         </div>
-        <button type="button" onClick={props.onHide} className="close-modal">
-          <FaTimesCircle />
-        </button>
-        <hr />
+        <div className="modal-body">
+          <p>{props.text}</p>
+        </div>
         {props.show && props.children}
       </div>
     </div>
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 const ModalContainer = styled.section`
-  .modal-block-false {
+  &.modal-block-false {
     display: none;
   }
 
-  .modal-block-true {
+  &.modal-block-true {
     text-align: center;
-    position: absolute;
+    position: fixed;
     left: 0;
     bottom: 0;
     left: 0;
@@ -16,7 +16,7 @@ const ModalContainer = styled.section`
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     display: flex;
-    animation: modal-background 0.3s ease-in-out;
+    animation: modal-background 0.2s ease-in-out;
     transform-origin: top;
   }
 
@@ -29,21 +29,36 @@ const ModalContainer = styled.section`
     }
   }
 
-  .modal-content {
-    width: 400rem;
-    max-width: calc(100% - 6rem);
+  .modal {
+    max-width: calc(100% - 2rem);
     margin: auto;
     display: flex;
     flex-direction: column;
-    background-color: #1e2939;
-    border-radius: 8px;
-    border: 2px solid ${({ theme }) => theme.colors.mainColor};
-    color: white;
+    background-color: ${({ theme }) => theme.colors.modalBackrgound};
+    border-radius: 20px;
+    color: ${({ theme }) => theme.colors.themeColor};
     text-align: left;
     padding: 2rem;
-    animation: modal-start 0.7s ease-in-out 0.3s forwards;
+    animation: modal-start 0.5s ease-in-out 0.2s forwards;
     transform-origin: center;
     transform: scale(0);
+
+    .modal-content {
+      .modal-title {
+        padding-top: 4rem;
+        font: 2rem ${({ theme }) => theme.fonts.primary};
+        text-align: center;
+        font-weight: bold;
+      }
+
+      .modal-body {
+        padding: 3rem;
+        padding-top: 2rem;
+        text-align: center;
+        font: 1.6rem ${({ theme }) => theme.fonts.primary};
+        color: ${({ theme }) => theme.colors.themeColor};
+      }
+    }
   }
 
   @keyframes modal-start {
@@ -55,61 +70,33 @@ const ModalContainer = styled.section`
     }
   }
 
-  .modal-title {
-    max-width: 90%;
-    font: 2rem ${({ theme }) => theme.fonts.primary}, sans-serif;
-    padding: 2px 7px;
-    text-shadow: 2px 2px 2px #030447d3;
-    letter-spacing: 1px;
-    font-weight: bold;
-  }
-
-  .modal-body {
-    padding: 14px 0;
-    font: 16px ${({ theme }) => theme.fonts.primary}, sans-serif;
-    color: gainsboro;
-  }
-
   .close-modal {
     border: none;
     background-color: transparent;
-    color: ${({ theme }) => theme.colors.mainColor};
+    color: ${({ theme }) => theme.colors.themeColor};
     font-size: 2rem;
-    display: flex;
     position: absolute;
+    display: flex;
     align-self: flex-end;
     cursor: pointer;
   }
 
-  .link-buttons-block {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .link-buttons-block a {
-    color: ${({ theme }) => theme.colors.namesakeText};
-    padding: 15px 15px;
-    background: ${({ theme }) => theme.colors.mainColor};
-    border-radius: 4px;
-    width: 48%;
-    letter-spacing: 1px;
-    transition: filter 1s ease, color 1s ease;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    font: 400 12px ${({ theme }) => theme.fonts.display}, sans-serif;
-  }
-
-  .link-buttons-block a:hover {
-    filter: drop-shadow(1px 1px 4px ${({ theme }) => theme.colors.mainColor});
-    color: ${({ theme }) => theme.colors.lightText};
-  }
-
   @media (min-width: 1100px) {
-    .modal-content {
-      width: 40%;
+    .modal {
+      width: 50%;
+      padding: 3rem;
+
+      .modal-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+        padding: 4rem;
+
+        .modal-title {
+          padding: 0;
+        }
+      }
     }
   }
 `;
