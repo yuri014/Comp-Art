463f95cc10fb3150b809fd252d6fbb2c6f4c3538
yuri014 <yuriflores2012@gmail.com>
2021-04-18 21:42:00 -0300

    put tags component inside styles components folder

--- src/styles/components/tags.ts
+++ src/styles/components/tags.ts
@@ -4,19 +4,19 @@ const TagsContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
-  font-size: 1.2rem;
-  & * {
-    margin-top: 2rem;
-    margin-right: 2rem;
+  gap: 1rem;
+
+  button {
+    font-size: 1.4rem;
     cursor: pointer;
     padding: 1rem;
     border: none;
-    background-color: ${({ theme }) => theme.colors.lightContrast};
-    color: ${({ theme }) => theme.colors.backgroundColor};
+    background-color: ${({ theme }) => theme.colors.pink};
+    color: ${({ theme }) => theme.colors.themeColor};
     font-weight: 700;
-    border-radius: 4px;
-    text-shadow: 0 0 2px #00000050;
+    border-radius: 5px;
     transition: all 0.4s ease;
+
     &:hover {
       filter: brightness(0.4);
     }
