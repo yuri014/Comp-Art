4ce687e3f319e8ee5413b0c2954f056ca24c34a4
yuri014 <yuriflores2012@gmail.com>
2021-04-20 22:15:24 -0300

    fix loading for new layout

--- src/components/ModalProfile/Loading.tsx
+++ src/components/ModalProfile/Loading.tsx
@@ -4,12 +4,23 @@ import Skeleton from '@material-ui/lab/Skeleton';
 const LoadingProfileLikes: React.FC = () => (
   <>
     <div className="profile">
-      <Skeleton animation="wave" variant="circle" height={50} width={50} />
-      <div className="profile-content">
-        <div className="profile-info">
-          <Skeleton animation="wave" variant="text" height={20} width={120} />
+      <div className="loading">
+        <Skeleton animation="wave" variant="circle" height={50} width={50} />
+        <div className="profile-content">
+          <div className="first-row">
+            <Skeleton animation="wave" variant="text" height={20} width={100} />
+          </div>
+          <div className="second-row">
+            <div className="bio">
+              <Skeleton
+                animation="wave"
+                height={30}
+                width={180}
+                component="p"
+              />
+            </div>
+          </div>
         </div>
-        <Skeleton className="bio" animation="wave" height={40} width={600} />
       </div>
     </div>
   </>
