78bf6934f0cac897760cd763caacb6fff3e47c0d
yuri014 <yuriflores2012@gmail.com>
2021-04-20 22:14:51 -0300

    adding secondary button

---
+++ src/styles/components/secondaryButton.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const CASecondaryButton = styled.button`
+  color: ${({ theme }) => theme.colors.pink};
+  font-weight: bold;
+  width: 9rem;
+  margin: auto 0;
+  cursor: pointer;
+  border: 1px solid ${({ theme }) => theme.colors.pink};
+  background-color: transparent;
+  border-radius: 30px;
+  padding: 0.4rem 1rem;
+  font-size: 1.2rem;
+  transition: 0.2s ease-out;
+
+  &.main-color {
+    color: ${({ theme }) => theme.colors.mainColor};
+    border: 1px solid ${({ theme }) => theme.colors.mainColor};
+
+    &:hover,
+    &.active {
+      background-color: ${({ theme }) => theme.colors.mainColor};
+      color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
+    }
+  }
+
+  &:hover,
+  &.active {
+    background-color: ${({ theme }) => theme.colors.pink};
+    color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
+  }
+`;
+
+export default CASecondaryButton;
