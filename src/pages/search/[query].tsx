bf2c6b8244d292820e1561c9e1d1d2c53dbfc510
yuri014 <yuriflores2012@gmail.com>
2021-04-24 13:33:47 -0300

    adding server side props to fetch profile search result

--- src/pages/search/[query].tsx
+++ src/pages/search/[query].tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useLazyQuery, useQuery } from '@apollo/client';
+import { gql, useLazyQuery, useQuery } from '@apollo/client';
 
+import { GetServerSideProps } from 'next';
 import Header from '../../components/Header';
 import MobileHeader from '../../components/MobileHeader';
 import MobileFooter from '../../components/MobileFooter';
 import Home from '../../components/Home';
 import LevelContext from '../../context/level';
-import { ILoggedProfile } from '../../interfaces/Profile';
+import { ILoggedProfile, IProfile } from '../../interfaces/Profile';
 import {
   GET_LEVEL_XP,
   GET_LOGGED_PROFILE,
@@ -16,8 +17,25 @@ import {
 import HomeContainer from '../home/_styles';
 import ProfileSimpleCard from '../../components/ProfileCard';
 import SearchContainer from './styles';
+import { initializeApollo } from '../../graphql/apollo/config';
 
-const HomePage: React.FC = () => {
+const GET_SEARCH_PROFILE = gql`
+  query SearchProfiles($query: String!, $offset: Int!, $limit: Int!) {
+    searchProfiles(query: $query, offset: $offset, limit: $limit) {
+      bio
+      name
+      owner
+      _id
+      avatar
+    }
+  }
+`;
+
+interface SearchPageProps {
+  profiles: Array<IProfile>;
+}
+
+const SearchPage: React.FC<SearchPageProps> = ({ profiles }) => {
   const {
     query: { query },
   } = useRouter();
@@ -53,7 +71,9 @@ const HomePage: React.FC = () => {
                 <section className="profile-results">
                   <p className="title">Resultados para &quot;{query}&quot;</p>
                   <div className="profiles-container">
-                    <ProfileSimpleCard profile={profileData.getLoggedProfile} />
+                    {profiles.map(profile => (
+                      <ProfileSimpleCard profile={profile} />
+                    ))}
                   </div>
                 </section>
               </SearchContainer>
@@ -77,4 +97,24 @@ const HomePage: React.FC = () => {
   );
 };
 
-export default HomePage;
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { query } = context.params;
+
+  const client = initializeApollo();
+
+  const getProfiles = await client.query({
+    query: GET_SEARCH_PROFILE,
+    variables: { query, offset: 0, limit: 0 },
+    errorPolicy: 'ignore',
+  });
+
+  const profiles = getProfiles.data.searchProfiles;
+
+  return {
+    props: {
+      profiles,
+    },
+  };
+};
+
+export default SearchPage;
