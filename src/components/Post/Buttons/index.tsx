d8199e372093b21db072e52ed72b0349e09aeac1
yuri014 <yuriflores2012@gmail.com>
2021-05-08 18:15:07 -0300

    adding save post mutation

--- src/components/Post/Buttons/index.tsx
+++ src/components/Post/Buttons/index.tsx
@@ -7,6 +7,7 @@ import {
   FaShareAlt,
 } from 'react-icons/fa';
 import Link from 'next/link';
+import { gql, useMutation } from '@apollo/client';
 
 interface PostInteractionButtonsProps {
   isLiked: boolean;
@@ -15,46 +16,61 @@ interface PostInteractionButtonsProps {
   postID: string;
 }
 
+const SAVE_POST = gql`
+  mutation AddSavedPost($id: ID!) {
+    addSavedPost(postID: $id)
+  }
+`;
+
 const PostInteractionButtons: React.FC<PostInteractionButtonsProps> = ({
   dislikePost,
   isLiked,
   likePost,
   postID,
-}) => (
-  <div className="post-interaction">
-    <div className="interaction-group">
+}) => {
+  const [savePost] = useMutation(SAVE_POST);
+
+  return (
+    <div className="post-interaction">
+      <div className="interaction-group">
+        <button
+          className={isLiked ? 'active' : ''}
+          type="button"
+          onClick={() => (isLiked ? dislikePost() : likePost())}
+          aria-label="Curtir"
+        >
+          <div className="interactions-button">
+            {isLiked ? <FaHeart size={20} /> : <FaRegHeart size={20} />}
+            <p>Curtir</p>
+          </div>
+        </button>
+        <Link href={`/post/${postID}`}>
+          <a>
+            <button aria-label="Comentar" type="button">
+              <div className="interactions-button">
+                <FaCommentAlt size={20} /> <p>Comentar</p>
+              </div>
+            </button>
+          </a>
+        </Link>
+        <button aria-label="Compartilhar" type="button">
+          <div className="interactions-button">
+            <FaShareAlt size={20} /> <p>Compartilhar</p>
+          </div>
+        </button>
+      </div>
       <button
-        className={isLiked ? 'active' : ''}
+        className="bookmark"
+        aria-label="Salvar"
         type="button"
-        onClick={() => (isLiked ? dislikePost() : likePost())}
-        aria-label="Curtir"
+        onClick={() => savePost({ variables: { id: postID } })}
       >
         <div className="interactions-button">
-          {isLiked ? <FaHeart size={20} /> : <FaRegHeart size={20} />}
-          <p>Curtir</p>
-        </div>
-      </button>
-      <Link href={`/post/${postID}`}>
-        <a>
-          <button aria-label="Comentar" type="button">
-            <div className="interactions-button">
-              <FaCommentAlt size={20} /> <p>Comentar</p>
-            </div>
-          </button>
-        </a>
-      </Link>
-      <button aria-label="Compartilhar" type="button">
-        <div className="interactions-button">
-          <FaShareAlt size={20} /> <p>Compartilhar</p>
+          <FaRegBookmark size={20} /> <p>Salvar</p>
         </div>
       </button>
     </div>
-    <button className="bookmark" aria-label="Salvar" type="button">
-      <div className="interactions-button">
-        <FaRegBookmark size={20} /> <p>Salvar</p>
-      </div>
-    </button>
-  </div>
-);
+  );
+};
 
 export default PostInteractionButtons;
