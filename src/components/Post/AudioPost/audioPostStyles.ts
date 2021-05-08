726ceb43de8cb45121a3561b12f9df99862588c3
yuri014 <yuriflores2012@gmail.com>
2021-05-08 15:33:01 -0300

    adding colors props to post styles

--- src/components/Post/AudioPost/audioPostStyles.ts
+++ src/components/Post/AudioPost/audioPostStyles.ts
@@ -1,17 +1,32 @@
 import styled from 'styled-components';
 
-const AudioPostContainer = styled.section`
+interface AudioPostContainerProps {
+  darkColor: string;
+  lightColor: string;
+}
+
+const AudioPostContainer = styled.section<AudioPostContainerProps>`
   margin: 0 auto;
   margin-bottom: 2rem;
 
   .audio-card {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: space-between;
     background: ${({ theme }) => theme.colors.lightSecondaryBackground};
     border-radius: 5px;
-    height: 21rem;
-    border-top: 1rem solid ${({ theme }) => theme.colors.mainColor};
+    height: 40rem;
+    border-top: 1rem solid ${props => props.darkColor};
+
+    .thumbnail {
+      img {
+        width: 20rem;
+        height: 20rem;
+        border-radius: 5px;
+        margin-top: 1rem;
+      }
+    }
 
     .audio-card-content {
       padding: 1rem 2rem;
@@ -25,8 +40,15 @@ const AudioPostContainer = styled.section`
         display: block;
       }
 
+      .audio-card-info {
+        a {
+          text-align: center;
+          width: 100%;
+        }
+      }
+
       .audio-buttons {
-        margin-top: 2rem;
+        margin-top: 1rem;
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -57,7 +79,7 @@ const AudioPostContainer = styled.section`
   }
 
   span {
-    color: ${({ theme }) => theme.colors.mainColor};
+    color: ${props => props.darkColor};
   }
 
   p {
@@ -77,18 +99,34 @@ const AudioPostContainer = styled.section`
     &.artist-name {
       font-weight: 400;
       font-size: 1.4rem;
-      color: #4bf1e3;
+      color: ${props => props.darkColor};
     }
   }
 
   @media (min-width: 1100px) {
     .audio-card {
-      .audio-buttons,
-      .progress {
-        padding: 1rem 4rem;
+      flex-direction: row;
+      height: 21rem;
+
+      .thumbnail {
+        img {
+          width: 16rem;
+          height: 14rem;
+          margin: 0;
+          margin-left: 2rem;
+        }
       }
 
       .audio-card-content {
+        padding: 3rem 2rem;
+
+        .audio-card-info {
+          a {
+            text-align: left;
+            padding: 0;
+          }
+        }
+
         .progress {
           .duration {
             transform: translateY(-1.2rem);
