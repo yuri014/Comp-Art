f8c8afadf3f16bc02a65ae154fdf36d87be17b8e
yuri014 <yuriflores2012@gmail.com>
2021-05-09 01:06:35 -0300

    share placeholder

---
+++ src/components/Share/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { PostProps } from '@interfaces/Post';
+
+const Share: React.FC<PostProps> = ({ post }) => (
+  <div>
+    <div className="share">TEMP</div>
+  </div>
+);
+
+export default Share;
