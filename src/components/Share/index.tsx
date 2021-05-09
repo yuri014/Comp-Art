08d9df67db6bf777400e6e26b5cbcf8497e480ee
yuri014 <yuriflores2012@gmail.com>
2021-05-09 17:43:06 -0300

    adding clickable link to post page

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -3,32 +3,35 @@ import React from 'react';
 import { PostProps } from '@interfaces/Post';
 import AuthorInfo from '@components/Post/utils/AuthorInfo';
 import ArtistPost from '@components/Post/ArtistPost';
+import usePostAsLink from '@hooks/postAsLink';
 import publishDate from '@utils/publishDate';
-import Link from 'next/link';
 import ShareContainer from './styles';
 
-const Share: React.FC<PostProps> = ({ post }) => (
-  <Link href={`/post/${post._id}`}>
-    <a>
-      <ShareContainer>
-        <div className="share">
-          <div className="share-info">
-            <AuthorInfo
-              postID={post._id}
-              profile={post.post.artist}
-              handleDeletePost={() => console.log('a')}
-            />
-          </div>
-          <div className="share-content">
-            <ArtistPost post={post.post} />
-            <div className="date">
-              <p>{publishDate(post.post.createdAt)}</p>
-            </div>
+const Share: React.FC<PostProps> = ({ post }) => {
+  const handlePostLink = usePostAsLink(post.post._id);
+
+  return (
+    <ShareContainer role="button" tabIndex={0} onClick={e => handlePostLink(e)}>
+      <div className="share">
+        <div className="share-info">
+          <AuthorInfo
+            postID={post._id}
+            profile={post.post.artist}
+            handleDeletePost={() => console.log('a')}
+          />
+        </div>
+        <div className="share-content">
+          {post.post.description && (
+            <div className="description">{post.post.description}</div>
+          )}
+          <ArtistPost post={post.post} />
+          <div className="date">
+            <p>{publishDate(post.post.createdAt)}</p>
           </div>
         </div>
-      </ShareContainer>
-    </a>
-  </Link>
-);
+      </div>
+    </ShareContainer>
+  );
+};
 
 export default Share;
