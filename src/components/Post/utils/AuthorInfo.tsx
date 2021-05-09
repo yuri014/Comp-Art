cb5b526a87ab711ea1ef0d040954c01cecbc4f04
yuri014 <yuriflores2012@gmail.com>
2021-05-09 01:00:18 -0300

    splitting author info component

---
+++ src/components/Post/utils/AuthorInfo.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
+
+import { IProfile } from '@interfaces/Profile';
+
+const OptionsMenu = dynamic(() => import('../OptionsMenu'));
+
+interface AuthorInfoProps {
+  profile: IProfile;
+  handleDeletePost: () => void;
+  postID: string;
+}
+
+const AuthorInfo: React.FC<AuthorInfoProps> = ({
+  handleDeletePost,
+  profile,
+  postID,
+}) => (
+  <>
+    <div className="author-info">
+      <img
+        alt={`Imagem de perfil de ${profile.name}`}
+        src={process.env.NEXT_PUBLIC_API_HOST + profile.avatar}
+      />
+      <Link href={`/profile/${profile.owner}`}>
+        <a>
+          <div>
+            <p>{profile.name}</p>
+            <span>
+              <p>@{profile.owner}</p>
+            </span>
+          </div>
+        </a>
+      </Link>
+    </div>
+    <div>
+      <OptionsMenu
+        deletePost={handleDeletePost}
+        id={postID}
+        username={profile.owner}
+      />
+    </div>
+  </>
+);
+
+export default AuthorInfo;
