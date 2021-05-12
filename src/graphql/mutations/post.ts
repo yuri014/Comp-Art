218f3c60b36d3c16d7b265527106d18339757b2e
yuri014 <yuriflores2012@gmail.com>
2021-05-11 22:50:01 -0300

    adding query to get who share the post

--- src/graphql/mutations/post.ts
+++ src/graphql/mutations/post.ts
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client';
+import { MODAL_PROFILE } from '@graphql/fragments/profile';
 
 export const CREATE_POST = gql`
   mutation CreatePost($post: CreatePostInput!) {
@@ -25,13 +26,19 @@ export const DELETE_POST = gql`
 `;
 
 export const GET_LIKES = gql`
+  ${MODAL_PROFILE}
   query GetLikes($id: ID!, $offset: Int!) {
     getLikes(postID: $id, offset: $offset) {
-      name
-      avatar
-      bio
-      owner
-      level
+      ...ModalProfile
+    }
+  }
+`;
+
+export const GET_WHO_SHARE_POST = gql`
+  ${MODAL_PROFILE}
+  query GetWhoSharesPost($id: ID!, $offset: Int!) {
+    getWhoSharesPost(postID: $id, offset: $offset) {
+      ...ModalProfile
     }
   }
 `;
