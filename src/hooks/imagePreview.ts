71af3d790bf090a58725332587e36f1db00d143c
yuri014 <yuriflores2012@gmail.com>
2021-05-13 00:45:19 -0300

    adding rule for cleaning the state

--- src/hooks/imagePreview.ts
+++ src/hooks/imagePreview.ts
@@ -5,6 +5,8 @@ interface ImagePreview {
   file: string | File;
 }
 
+type TypeSetImagePreview = React.ChangeEvent<HTMLInputElement> | string;
+
 /**
  * Hook para obter o preview e o arquivo de uma imagem.
  * @returns {Array} [image, setImage].
@@ -13,13 +15,27 @@ interface ImagePreview {
  */
 const useImagePreview = (): [
   ImagePreview,
-  (_args0: React.ChangeEvent<HTMLInputElement>) => void,
+  (_args0: TypeSetImagePreview) => void,
 ] => {
-  const [image, setImage] = useState<ImagePreview>({
+  const initialState = {
     preview: '',
     file: '',
-  });
-  const setImagePreview = (event: React.ChangeEvent<HTMLInputElement>) => {
+  };
+
+  const [image, setImage] = useState<ImagePreview>(initialState);
+
+  /**
+   * Aplica lógia do FileReader antes de setar o estado.
+   * @param {string} string para limpar o campo.
+   * @param {event} event evento de um HTMLInputElement.
+   */
+  const setImagePreview = (event: TypeSetImagePreview) => {
+    if (typeof event === 'string') {
+      setImage(initialState);
+
+      return;
+    }
+
     const reader = new FileReader();
 
     const file = event.target.files[0];
