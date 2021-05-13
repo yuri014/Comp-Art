10e3e96e96708516d2e38186f4ab3c6b08347bc1
yuri014 <yuriflores2012@gmail.com>
2021-05-12 22:39:42 -0300

    create post style with buttons

--- src/components/CreatePost/styles.ts
+++ src/components/CreatePost/styles.ts
@@ -1,32 +1,104 @@
 import styled from 'styled-components';
 
 const CreatePostContainer = styled.div`
-  width: 100%;
-  padding: 1rem;
   margin-bottom: 2rem;
   background-color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
+  display: grid;
+  grid-template-columns: 5rem 80%;
+  justify-content: space-between;
+  padding: 2rem 1rem;
 
-  .editor {
-    display: flex;
-    align-items: flex-start;
-    gap: 1rem;
+  img {
+    width: 5rem;
+    height: 5rem;
+    border-radius: 50%;
+    object-fit: fill;
+  }
+
+  form {
+    width: 100%;
+    padding-right: 1rem;
 
     .hashtag {
       color: ${({ theme }) => theme.colors.hashtag};
     }
 
-    img {
-      width: 5rem;
-      height: 5rem;
-      border-radius: 50%;
-      object-fit: fill;
-    }
-
     .DraftEditor-root {
-      width: 85%;
+      width: 100%;
       font-size: 1.6rem;
       margin-top: 1rem;
+      padding-bottom: 0.4rem;
     }
+
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 1rem;
+      padding-top: 1rem;
+      border-top: 1px solid ${({ theme }) => theme.colors.darkGray};
+
+      .publish {
+        background: ${({ theme }) => theme.colors.pink};
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        padding: 1rem;
+        border-radius: 5px;
+        font-weight: 600;
+        transition: filter 0.225s ease-out;
+
+        &:focus,
+        &:hover {
+          filter: brightness(0.75);
+        }
+      }
+
+      div {
+        display: flex;
+
+        button {
+          background: transparent;
+          color: ${({ theme }) => theme.colors.mainColor};
+          font-size: 2rem;
+          margin-right: 1rem;
+          padding: 1rem;
+          border-radius: 50%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border: none;
+          cursor: pointer;
+          transition: background-color 0.225s ease-out;
+
+          &:focus,
+          &:hover {
+            background-color: ${({ theme }) => theme.colors.mainColor}20;
+          }
+
+          &:focus {
+            border: 2px solid ${({ theme }) => theme.colors.mainColor};
+            padding: calc(1rem - 2px);
+            outline: none;
+          }
+        }
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 2rem;
+    padding: 4rem;
+    border-radius: 5px;
+    grid-template-columns: 5rem 88%;
+  }
+
+  @media (min-width: 1100px) {
+    padding: 2rem;
+    width: 100%;
+    grid-template-columns: 5rem 90%;
   }
 `;
 
