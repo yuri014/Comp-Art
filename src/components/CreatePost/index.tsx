263a262d4e09d75186b447135aab6eb67088ca74
yuri014 <yuriflores2012@gmail.com>
2021-05-15 04:50:08 -0300

    adding mutation to submit

--- src/components/CreatePost/index.tsx
+++ src/components/CreatePost/index.tsx
@@ -6,6 +6,8 @@ import usePreventMemoryLeak from '@hooks/preventMemoryLeak';
 import useImageDimension from '@hooks/imageDimension';
 import { IProfile } from '@interfaces/Profile';
 import useImagePreview from '@hooks/imagePreview';
+import { CREATE_POST } from '@graphql/mutations/post';
+import { useMutation } from '@apollo/client';
 import CreatePostContainer from './styles';
 import DraftEditor from './DraftEditor';
 import 'draft-js/dist/Draft.css';
@@ -22,16 +24,26 @@ const CreatePost: React.FC<CreatePostProps> = ({ getLoggedProfile }) => {
   const [audioResult, setAudioResult] = useState<File>();
   const imageDimension = useImageDimension(imagePreview.preview);
 
+  const [createPost] = useMutation(CREATE_POST, {
+    onError: ({ graphQLErrors }) => console.log(graphQLErrors[0].message),
+  });
+
   const onSubmit = () => {
     if (description.length >= 1200) {
       console.log('error');
+
+      return;
     }
 
-    console.log({
-      description,
-      body: audioResult || imagePreview.file,
-      alt: '',
-      thumbnail: audioResult ? imagePreview.file : '',
+    createPost({
+      variables: {
+        post: {
+          description,
+          body: audioResult || imagePreview.file,
+          alt: '',
+          thumbnail: audioResult ? imagePreview.file : '',
+        },
+      },
     });
   };
 
