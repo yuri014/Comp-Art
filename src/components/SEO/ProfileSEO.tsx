fefbf988750a4b7bcbec3145af8776d151e6d2ed
yuri014 <yuriflores2012@gmail.com>
2021-05-15 21:54:54 -0300

    adding profile meta

---
+++ src/components/SEO/ProfileSEO.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { IProfile } from '@interfaces/Profile';
+import formatMetaHashtags from '@utils/formatHashtags';
+import Meta from './Meta';
+
+interface ProfileSEOProps {
+  getProfile: IProfile;
+}
+
+const ProfileSEO: React.FC<ProfileSEOProps> = ({ getProfile }) => {
+  const hashtags = formatMetaHashtags(getProfile.hashtags);
+
+  return (
+    <Meta
+      title={`${getProfile.owner} - Perfil`}
+      description={`${getProfile.owner} é um ${
+        getProfile.isArtist
+          ? `artista que produz e/ou se interessa por ${hashtags}`
+          : `fã de ${hashtags}`
+      }`}
+      keywords={`${getProfile.owner}, ${getProfile.name}, ${hashtags}, ${
+        getProfile.bio
+      }, ${getProfile.isArtist ? 'artista' : 'fã'}`}
+      uri={`profile/${getProfile.owner}`}
+      seoImage={getProfile.avatar}
+    />
+  );
+};
+
+export default ProfileSEO;
