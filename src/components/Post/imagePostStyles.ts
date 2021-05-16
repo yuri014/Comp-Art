bd1cfae4bc7bf247c2e659a1a32d587404159cb3
yuri014 <yuriflores2012@gmail.com>
2021-05-15 21:01:57 -0300

    refactoring styles for imageHeight

--- src/components/Post/imagePostStyles.ts
+++ src/components/Post/imagePostStyles.ts
@@ -14,18 +14,8 @@ const ImagePostContainer = styled.div`
     .post-image {
       cursor: pointer;
       width: 100%;
-      height: 30vh;
-
-      @media (min-width: 1100px) {
-        height: 45vh;
-      }
-
-      img {
-        width: 100%;
-        height: 100%;
-        border-radius: 4px;
-        object-fit: cover;
-      }
+      border-radius: 4px;
+      object-fit: cover;
     }
   }
 `;
