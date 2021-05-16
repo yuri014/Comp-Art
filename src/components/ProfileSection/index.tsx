bd49b1e33e7a341d33be58c0e137018c00353397
yuri014 <yuriflores2012@gmail.com>
2021-05-15 23:28:13 -0300

    splitting profile section component

---
+++ src/components/ProfileSection/index.tsx
@@ -0,0 +1,125 @@
+import React, { useContext, useState } from 'react';
+import Link from 'next/link';
+
+import { useMutation, useQuery } from '@apollo/client';
+import { AuthContext } from '@context/auth';
+import { FOLLOW_PROFILE, UNFOLLOW_PROFILE } from '@graphql/mutations/profile';
+import { IProfile } from '@interfaces/Profile';
+import CAImage from '@components/CAImage';
+import { GET_IS_FOLLOWING } from '@graphql/queries/profile';
+import formatDate from '@utils/formatDate';
+import ProfileSectionContainer from './_styles';
+
+interface ProfileProps {
+  getProfile: IProfile;
+}
+
+const ProfileSection: React.FC<ProfileProps> = ({ getProfile }) => {
+  const auth = useContext(AuthContext);
+  const [followersCount, setFollowersCount] = useState(getProfile.followers);
+  const [follow] = useMutation(FOLLOW_PROFILE);
+  const [unfollow] = useMutation(UNFOLLOW_PROFILE);
+
+  const [isFollowing, setIsFollowing] = useState(false);
+  const { data: getIsFollowing, loading } = useQuery(GET_IS_FOLLOWING, {
+    variables: {
+      id: getProfile._id,
+    },
+    onCompleted: () => setIsFollowing(getIsFollowing.getIsFollowing),
+  });
+
+  const hasAuth = auth.user;
+  const checkFollowButton = () =>
+    hasAuth && getProfile.owner !== auth.user.username;
+
+  return (
+    <ProfileSectionContainer>
+      <div className="avatar-profile">
+        <CAImage image={getProfile.avatar} />
+        <p>Level {getProfile.level}</p>
+      </div>
+      <div className="profile">
+        <div>
+          <h1 title={getProfile.name}>{getProfile.name}</h1>
+          <h2>@{getProfile.owner}</h2>
+        </div>
+        <div className="profile-numbers">
+          <div>
+            {getProfile.sharedPostCount !== null && (
+              <p>{getProfile.sharedPostCount}</p>
+            )}
+            {getProfile.postCount !== null && <p>{getProfile.postCount}</p>}
+            <p>Publicações </p>
+          </div>
+          <div>
+            <p>{getProfile.following}</p>
+            <p>Seguindo</p>
+          </div>
+          <div>
+            <p>{followersCount}</p>
+            <p>Seguidores</p>
+          </div>
+        </div>
+      </div>
+      <div className="buttons-profile">
+        <button type="button" className="sponsorship">
+          PATROCINAR
+        </button>
+        {hasAuth && getProfile.owner === auth.user.username && (
+          <Link href="/profile/edit">
+            <a>
+              <button className="edit-profile" type="button">
+                EDITAR PERFIL
+              </button>
+            </a>
+          </Link>
+        )}
+
+        {checkFollowButton() && isFollowing && !loading && (
+          <button
+            className="main-color"
+            type="button"
+            onClick={() => {
+              unfollow({
+                variables: { username: getProfile.owner },
+              });
+              setIsFollowing(false);
+              setFollowersCount(followersCount - 1);
+            }}
+          >
+            SEGUINDO
+          </button>
+        )}
+        {checkFollowButton() && !isFollowing && !loading && (
+          <button
+            type="button"
+            onClick={() => {
+              follow({
+                variables: { username: getProfile.owner },
+              });
+              setIsFollowing(true);
+              setFollowersCount(followersCount + 1);
+            }}
+          >
+            SEGUIR
+          </button>
+        )}
+        {!hasAuth && (
+          <button className="main-color" type="button">
+            SEGUIR
+          </button>
+        )}
+      </div>
+      {getProfile.bio && (
+        <div className="bio">
+          <p>{getProfile.bio}</p>
+        </div>
+      )}
+      <div className="joined">
+        <p>Ingressou em {formatDate(getProfile.createdAt)}</p>
+      </div>
+    </ProfileSectionContainer>
+  );
+};
+
+export default ProfileSection;
--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,8 +1,7 @@
-import React, { useContext, useState } from 'react';
-import { useMutation, useQuery } from '@apollo/client';
+import React, { useContext } from 'react';
+import { useQuery } from '@apollo/client';
 import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import { ThemeProvider } from '@material-ui/core';
 
 import Header from '@components/Header';
@@ -10,16 +9,10 @@ import MobileFooter from '@components/MobileFooter';
 import Post from '@components/Post';
 import SkeletonPost from '@components/Post/SkeletonPost';
 import ProfileLinks from '@components/Splitter/ProfileLinks';
-import { AuthContext } from '@context/auth';
 import ThemeContext from '@context/theme';
 import { initializeApollo } from '@graphql/apollo/config';
-import { FOLLOW_PROFILE, UNFOLLOW_PROFILE } from '@graphql/mutations/profile';
 import { GET_PROFILE_POSTS } from '@graphql/queries/post';
-import {
-  GET_IS_FOLLOWING,
-  GET_PROFILE,
-  GET_LOGGED_PROFILE,
-} from '@graphql/queries/profile';
+import { GET_PROFILE, GET_LOGGED_PROFILE } from '@graphql/queries/profile';
 import useInfiniteScroll from '@hooks/infiniteScroll';
 import { Timeline } from '@interfaces/Post';
 import { ILoggedProfile, IProfile } from '@interfaces/Profile';
@@ -27,7 +20,7 @@ import mainDarkTheme from '@styles/themes/MainDarkTheme';
 import mainLightTheme from '@styles/themes/MainLightTheme';
 import ArtistPost from '@components/Post/ArtistPost';
 import ProfileSEO from '@components/SEO/ProfileSEO';
-import formatDate from '@utils/formatDate';
+import ProfileSection from '@components/ProfileSection';
 import ProfileContainer from './_styles';
 
 const CAImage = dynamic(() => import('@components/CAImage'));
@@ -42,18 +35,7 @@ const Profile: React.FC<ProfileProps> = ({
   getProfile,
   getLoggedProfile,
 }) => {
-  const auth = useContext(AuthContext);
   const { theme } = useContext(ThemeContext);
-  const [isFollowing, setIsFollowing] = useState(false);
-  const { data: getIsFollowing, loading } = useQuery(GET_IS_FOLLOWING, {
-    variables: {
-      id: getProfile._id,
-    },
-    onCompleted: () => setIsFollowing(getIsFollowing.getIsFollowing),
-  });
-
-  const [follow] = useMutation(FOLLOW_PROFILE);
-  const [unfollow] = useMutation(UNFOLLOW_PROFILE);
 
   const { client, data, error, loading: loadingPost, fetchMore } = useQuery<{
     getProfilePosts: Array<Timeline>;
@@ -78,13 +60,6 @@ const Profile: React.FC<ProfileProps> = ({
       }).then(newPosts => newPosts.data.getProfilePosts.length < 3),
   );
 
-  const hasAuth = auth.user;
-
-  const [followersCount, setFollowersCount] = useState(getProfile.followers);
-
-  const checkFollowButton = () =>
-    hasAuth && getProfile.owner !== auth.user.username;
-
   const hasCoverProfile = getProfile.coverImage !== '';
 
   return (
@@ -92,112 +67,16 @@ const Profile: React.FC<ProfileProps> = ({
       <ProfileSEO getProfile={getProfile} />
       <Header getLoggedProfile={getLoggedProfile} />
       <ProfileContainer>
-        <main>
-          <div
-            className={`cover-profile ${
-              hasCoverProfile ? '' : 'cover-placeholder'
-            }`}
-          >
-            {hasCoverProfile && <CAImage image={getProfile.coverImage} />}
+        <div
+          className={`cover-profile ${
+            hasCoverProfile ? '' : 'cover-placeholder'
+          }`}
+        >
+          {hasCoverProfile && <CAImage image={getProfile.coverImage} />}
 
-            <ProfileLinks links={getProfile.links} />
-          </div>
-          <section>
-            <div className="avatar-profile">
-              <CAImage image={getProfile.avatar} />
-              <p>Level {getProfile.level}</p>
-            </div>
-            <div className="profile">
-              <div>
-                <h1 title={getProfile.name}>{getProfile.name}</h1>
-                <h2>@{getProfile.owner}</h2>
-              </div>
-              <div className="profile-numbers">
-                <div>
-                  {getProfile.sharedPostCount !== null && (
-                    <p>{getProfile.sharedPostCount}</p>
-                  )}
-                  {getProfile.postCount !== null && (
-                    <p>{getProfile.postCount}</p>
-                  )}
-                  <p>Publicações </p>
-                </div>
-                <div>
-                  <p>{getProfile.following}</p>
-                  <p>Seguindo</p>
-                </div>
-                <div>
-                  <p>{followersCount}</p>
-                  <p>Seguidores</p>
-                </div>
-              </div>
-            </div>
-            {getProfile.bio && (
-              <div className="bio">
-                <p>{getProfile.bio}</p>
-              </div>
-            )}
-            <div className="buttons-profile">
-              <button type="button" className="sponsorship">
-                PATROCINAR
-              </button>
-              {hasAuth && getProfile.owner === auth.user.username && (
-                <Link href="/profile/edit">
-                  <a>
-                    <button className="edit-profile" type="button">
-                      EDITAR PERFIL
-                    </button>
-                  </a>
-                </Link>
-              )}
-
-              {checkFollowButton() && isFollowing && !loading && (
-                <button
-                  className="main-color"
-                  type="button"
-                  onClick={() => {
-                    unfollow({
-                      variables: { username: getProfile.owner },
-                    });
-                    setIsFollowing(false);
-                    setFollowersCount(followersCount - 1);
-                  }}
-                >
-                  SEGUINDO
-                </button>
-              )}
-              {checkFollowButton() && !isFollowing && !loading && (
-                <button
-                  type="button"
-                  onClick={() => {
-                    follow({
-                      variables: { username: getProfile.owner },
-                    });
-                    setIsFollowing(true);
-                    setFollowersCount(followersCount + 1);
-                  }}
-                >
-                  SEGUIR
-                </button>
-              )}
-              {!hasAuth && (
-                <button className="main-color" type="button">
-                  SEGUIR
-                </button>
-              )}
-            </div>
-            <div className="bio">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-                dicta deserunt incidunt esse deleniti eius perspiciatis illum
-                adipisci dolores. Adipisci.
-              </p>
-            </div>
-            <div className="joined">
-              <p>Ingressou em {formatDate(getProfile.createdAt)}</p>
-            </div>
-          </section>
-        </main>
+          <ProfileLinks links={getProfile.links} />
+        </div>
+        <ProfileSection getProfile={getProfile} />
         <section className="profile-posts">
           {loadingPost || error ? (
             <SkeletonPost />
