0bdac38e61165e528e1728199274dd9278960b67
yuri014 <yuriflores2012@gmail.com>
2021-05-19 21:57:34 -0300

    fix theme context in loading

--- src/components/Post/SkeletonPost.tsx
+++ src/components/Post/SkeletonPost.tsx
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { ThemeProvider } from '@material-ui/core';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
-import mainTheme from '../../styles/themes/MainTheme';
+import ThemeContext from '@context/theme';
+import mainLightTheme from '@styles/themes/MainLightTheme';
+import mainDarkTheme from '@styles/themes/MainDarkTheme';
 import PostContainer from './styles';
 
-const SkeletonPost: React.FC = () => (
-  <PostContainer>
-    <ThemeProvider theme={mainTheme}>
-      <div className="post-author">
-        <div className="author-info">
-          <Skeleton animation="wave" variant="circle" width={60} height={60} />
-          <div>
-            <Skeleton animation="wave" width={150} height={24} />
-            <span>
-              <Skeleton
-                animation="wave"
-                width={120}
-                height={24}
-                component="p"
-              />
-            </span>
+const SkeletonPost: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <PostContainer>
+      <ThemeProvider theme={theme === 'light' ? mainLightTheme : mainDarkTheme}>
+        <div className="post-author">
+          <div className="author-info">
+            <Skeleton
+              animation="wave"
+              variant="circle"
+              width={60}
+              height={60}
+            />
+            <div>
+              <Skeleton animation="wave" width={150} height={24} />
+              <span>
+                <Skeleton
+                  animation="wave"
+                  width={120}
+                  height={24}
+                  component="p"
+                />
+              </span>
+            </div>
+          </div>
+          <div className="post-config">
+            <FiMoreHorizontal />
           </div>
         </div>
-        <div className="post-config">
-          <FiMoreHorizontal />
+        <div className="post">
+          <div className="post-description-loading">
+            <Skeleton animation="wave" width="100%" height={60} />
+          </div>
+          <Skeleton animation="wave" variant="rect" width="100%" height={378} />
         </div>
-      </div>
-      <div className="post">
-        <div className="post-description-loading">
-          <Skeleton animation="wave" width="100%" height={60} />
-        </div>
-        <Skeleton animation="wave" variant="rect" width="100%" height={378} />
-      </div>
-    </ThemeProvider>
-  </PostContainer>
-);
+      </ThemeProvider>
+    </PostContainer>
+  );
+};
 
 export default SkeletonPost;
