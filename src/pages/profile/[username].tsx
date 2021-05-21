a60768b701cd98c9c7483e2a15cb3769d71b0a9f
yuri014 <yuriflores2012@gmail.com>
2021-05-20 22:42:54 -0300

    fix profile page

--- src/pages/profile/[username].tsx
+++ src/pages/profile/[username].tsx
@@ -1,24 +1,19 @@
 import React, { useContext } from 'react';
-import { useQuery } from '@apollo/client';
 import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
 import { ThemeProvider } from '@material-ui/core';
 
 import Header from '@components/Header';
 import MobileFooter from '@components/MobileFooter';
-import Post from '@components/Post';
-import SkeletonPost from '@components/Post/SkeletonPost';
 import ProfileLinks from '@components/Splitter/ProfileLinks';
 import ThemeContext from '@context/theme';
+import Timeline from '@components/Timeline';
 import { initializeApollo } from '@graphql/apollo/config';
 import { GET_PROFILE_POSTS } from '@graphql/queries/post';
 import { GET_PROFILE, GET_LOGGED_PROFILE } from '@graphql/queries/profile';
-import useInfiniteScroll from '@hooks/infiniteScroll';
-import { Timeline } from '@interfaces/Post';
 import { ILoggedProfile, IProfile } from '@interfaces/Profile';
 import mainDarkTheme from '@styles/themes/MainDarkTheme';
 import mainLightTheme from '@styles/themes/MainLightTheme';
-import ArtistPost from '@components/Post/ArtistPost';
 import ProfileSEO from '@components/SEO/ProfileSEO';
 import ProfileSection from '@components/ProfileSection';
 import Link from 'next/link';
@@ -39,29 +34,6 @@ const Profile: React.FC<ProfileProps> = ({
 }) => {
   const { isDarkMode } = useContext(ThemeContext);
 
-  const { client, data, error, loading: loadingPost, fetchMore } = useQuery<{
-    getProfilePosts: Array<Timeline>;
-  }>(GET_PROFILE_POSTS, {
-    variables: { offset: 0, username },
-    ssr: true,
-    fetchPolicy: 'cache-and-network',
-    nextFetchPolicy: 'cache-first',
-    onCompleted: () => {
-      if (data.getProfilePosts.length === 0) {
-        client.cache.evict({ fieldName: 'getProfilePosts' });
-      }
-    },
-  });
-
-  const lastPostRef = useInfiniteScroll(
-    data,
-    () =>
-      !!data.getProfilePosts &&
-      fetchMore({
-        variables: { offset: data.getProfilePosts.length },
-      }).then(newPosts => newPosts.data.getProfilePosts.length < 3),
-  );
-
   const hasCoverProfile = getProfile.coverImage !== '';
 
   return (
@@ -81,31 +53,11 @@ const Profile: React.FC<ProfileProps> = ({
         <main>
           <ProfileSection getProfile={getProfile} />
           <section className="profile-posts">
-            {loadingPost || error ? (
-              <SkeletonPost />
-            ) : (
-              data.getProfilePosts.map((post, index) => {
-                if (data.getProfilePosts.length === index + 1) {
-                  return (
-                    <div
-                      key={`${post.artist}_${post.createdAt}`}
-                      ref={lastPostRef}
-                    >
-                      <Post post={post}>
-                        <ArtistPost post={post} />
-                      </Post>
-                    </div>
-                  );
-                }
-                return (
-                  <div key={`${post.artist}_${post.createdAt}`}>
-                    <Post post={post}>
-                      <ArtistPost post={post} />
-                    </Post>
-                  </div>
-                );
-              })
-            )}
+            <Timeline
+              query={GET_PROFILE_POSTS}
+              queryName="getProfilePosts"
+              otherVariables={{ username }}
+            />
           </section>
           <section className="no-logged">
             <div>
