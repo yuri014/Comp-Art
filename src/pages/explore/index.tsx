86699eb1efa67906becbe0e380abca3f657b92e4
yuri014 <yuriflores2012@gmail.com>
2021-05-20 22:31:31 -0300

    fixing explore page

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,54 +1,21 @@
-import React, { useContext, useEffect } from 'react';
-import { useLazyQuery, useQuery } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
-import HomeProfile from '@components/HomeProfile';
 import MobileFooter from '@components/MobileFooter';
 import MobileHeader from '@components/MobileHeader';
-import Post from '@components/Post';
-import LoadingPost from '@components/Post/LoadingPost';
-import QuestsProgress from '@components/QuestsProgress';
-import { AuthContext } from '@context/auth';
 import LevelContext from '@context/level';
+import Timeline from '@components/Timeline';
 import { GET_EXPLORE_POSTS } from '@graphql/queries/post';
 import { GET_LOGGED_PROFILE, GET_LEVEL_XP } from '@graphql/queries/profile';
-import useInfiniteScroll from '@hooks/infiniteScroll';
-import { Timeline } from '@interfaces/Post';
+import { initializeApollo } from '@graphql/apollo/config';
+import { ILoggedProfile } from '@interfaces/Profile';
+import Home from '@components/Home';
+import Header from '@components/Header';
 import HomeContainer from '../home/_styles';
 
-interface IExplorePosts {
-  getExplorePosts: Array<Timeline>;
-}
-
-const Explore: React.FC = () => {
-  const { user } = useContext(AuthContext);
-  const { data, loading, error, fetchMore } = useQuery<IExplorePosts>(
-    GET_EXPLORE_POSTS,
-    {
-      variables: { offset: 0 },
-    },
-  );
-
-  const lastPostRef = useInfiniteScroll(
-    data,
-    () =>
-      !!data.getExplorePosts &&
-      fetchMore({
-        variables: { offset: data.getExplorePosts.length },
-      }).then(newPosts => newPosts.data.getExplorePosts.length < 3),
-  );
-
-  const [
-    getProfile,
-    { data: profileData, loading: loadingProfile },
-  ] = useLazyQuery(GET_LOGGED_PROFILE);
-
-  useEffect(() => {
-    if (user) {
-      getProfile();
-    }
-  }, [getProfile, user]);
-
+const Explore: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
   const [getLevel, { data: profileLevel }] = useLazyQuery(GET_LEVEL_XP, {
     fetchPolicy: 'no-cache',
   });
@@ -62,54 +29,54 @@ const Explore: React.FC = () => {
       <Head>
         <title>Comp-Art</title>
       </Head>
-      <div className="container">
-        <div className="home-desktop-content">
-          <div className="profile">
-            {profileData && (
-              <LevelContext.Provider
-                value={{ updateLevel: getLevel, level: profileLevel }}
-              >
-                <HomeProfile getLoggedProfile={profileData.getLoggedProfile} />
-              </LevelContext.Provider>
-            )}
-          </div>
-          <div className="timeline">
-            {loading || error ? (
-              <LoadingPost loading={loading} />
-            ) : (
-              data.getExplorePosts.map((post, index) => {
-                if (data.getExplorePosts.length === index + 1) {
-                  return (
-                    <div
-                      key={`${post.artist}_${post.createdAt}`}
-                      ref={lastPostRef}
-                    >
-                      <Post post={post} />
-                    </div>
-                  );
-                }
-                return (
-                  <div key={`${post.artist}_${post.createdAt}`}>
-                    <Post post={post} />
-                  </div>
-                );
-              })
-            )}
-          </div>
-          <div className="quests">
-            <QuestsProgress />
-          </div>
-        </div>
-      </div>
-      {profileData && (
-        <MobileHeader
-          loading={loadingProfile}
-          getLoggedProfile={profileData.getLoggedProfile}
-        />
-      )}
+      <Header getLoggedProfile={getLoggedProfile} />
+      <LevelContext.Provider
+        value={{ updateLevel: getLevel, level: profileLevel }}
+      >
+        <Home getLoggedProfile={getLoggedProfile}>
+          <Timeline query={GET_EXPLORE_POSTS} queryName="getExplorePosts" />
+        </Home>
+        <MobileHeader getLoggedProfile={getLoggedProfile} />
+      </LevelContext.Provider>
       <MobileFooter />
     </HomeContainer>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { jwtToken } = req.cookies;
+
+  if (!jwtToken) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const client = initializeApollo(null, jwtToken);
+
+  const getProfile = await client.query({
+    query: GET_LOGGED_PROFILE,
+    errorPolicy: 'ignore',
+  });
+
+  if (jwtToken && !getProfile.data) {
+    return {
+      redirect: {
+        destination: '/register-profile',
+        permanent: false,
+      },
+    };
+  }
+
+  const { getLoggedProfile } = getProfile.data;
+  return {
+    props: {
+      getLoggedProfile,
+    },
+  };
 };
 
 export default Explore;
