bf2b62a4c9055fdef61117cd23ea35e8a2e0ca16
yuri014 <yuriflores2012@gmail.com>
2021-05-20 21:26:50 -0300

    adding share mutation hook

---
+++ src/hooks/shareMutations.ts
@@ -0,0 +1,42 @@
+import { useMutation } from '@apollo/client';
+import { UseInteractionsMutation } from '@interfaces/Hooks';
+import {
+  DELETE_SHARE,
+  DISLIKE_SHARE,
+  LIKE_SHARE,
+} from '../graphql/mutations/share';
+
+const useSharesMutations: UseInteractionsMutation = (
+  id,
+  dislikeCallback,
+  likeCallback,
+) => {
+  const [deletePost] = useMutation(DELETE_SHARE, {
+    variables: { id },
+    update(cache) {
+      cache.modify({
+        fields: {
+          getPosts(existingPosts, { readField }) {
+            return existingPosts.filter(
+              postRef => id !== readField('_id', postRef),
+            );
+          },
+        },
+      });
+    },
+  });
+
+  const [dislikePost] = useMutation(DISLIKE_SHARE, {
+    variables: { id },
+    onCompleted: dislikeCallback,
+  });
+
+  const [likePost] = useMutation(LIKE_SHARE, {
+    variables: { id },
+    onCompleted: likeCallback,
+  });
+
+  return [deletePost, dislikePost, likePost];
+};
+
+export default useSharesMutations;
