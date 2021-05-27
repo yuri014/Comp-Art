40401dd3cad588cb311afb19a4a1c89d4da27e14
yuri014 <yuriflores2012@gmail.com>
2021-05-27 20:46:27 -0300

    adding theme provider in explore page

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
+import { ThemeProvider } from '@material-ui/core';
 
 import MobileFooter from '@components/MobileFooter';
 import MobileHeader from '@components/MobileHeader';
@@ -13,9 +14,13 @@ import { initializeApollo } from '@graphql/apollo/config';
 import { ILoggedProfile } from '@interfaces/Profile';
 import Home from '@components/Home';
 import Header from '@components/Header';
+import ThemeContext from '@context/theme';
+import mainDarkTheme from '@styles/themes/MainDarkTheme';
+import mainLightTheme from '@styles/themes/MainLightTheme';
 import HomeContainer from '../home/_styles';
 
 const Explore: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
+  const { isDarkMode } = useContext(ThemeContext);
   const [getLevel, { data: profileLevel }] = useLazyQuery(GET_LEVEL_XP, {
     fetchPolicy: 'no-cache',
   });
@@ -25,21 +30,23 @@ const Explore: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
   }, [getLevel]);
 
   return (
-    <HomeContainer>
-      <Head>
-        <title>Comp-Art</title>
-      </Head>
-      <Header getLoggedProfile={getLoggedProfile} />
-      <LevelContext.Provider
-        value={{ updateLevel: getLevel, level: profileLevel }}
-      >
-        <Home getLoggedProfile={getLoggedProfile}>
-          <Timeline query={GET_EXPLORE_POSTS} queryName="getExplorePosts" />
-        </Home>
-        <MobileHeader getLoggedProfile={getLoggedProfile} />
-      </LevelContext.Provider>
-      <MobileFooter />
-    </HomeContainer>
+    <ThemeProvider theme={isDarkMode ? mainDarkTheme : mainLightTheme}>
+      <HomeContainer>
+        <Head>
+          <title>Comp-Art</title>
+        </Head>
+        <Header getLoggedProfile={getLoggedProfile} />
+        <LevelContext.Provider
+          value={{ updateLevel: getLevel, level: profileLevel }}
+        >
+          <Home getLoggedProfile={getLoggedProfile}>
+            <Timeline query={GET_EXPLORE_POSTS} queryName="getExplorePosts" />
+          </Home>
+          <MobileHeader getLoggedProfile={getLoggedProfile} />
+        </LevelContext.Provider>
+        <MobileFooter />
+      </HomeContainer>
+    </ThemeProvider>
   );
 };
 
