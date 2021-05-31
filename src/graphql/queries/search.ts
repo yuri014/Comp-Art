b1f349ef746ae620d52caa7539041cc469e5dd55
yuri014 <yuriflores2012@gmail.com>
2021-05-31 20:18:36 -0300

    fixing search post query

--- src/graphql/queries/search.ts
+++ src/graphql/queries/search.ts
@@ -15,30 +15,32 @@ export const GET_SEARCH_PROFILE = gql`
 export const GET_SEARCH_POSTS = gql`
   query SearchPost($query: String!, $offset: Int!) {
     searchPost(query: $query, offset: $offset) {
-      _id
-      description
-      body
-      likes {
-        profile {
-          name
-          avatar
-          _id
-          owner
-          bio
+      ... on Post {
+        _id
+        description
+        body
+        likes {
+          profile {
+            name
+            avatar
+            _id
+            owner
+            bio
+          }
         }
+        likesCount
+        sharedCount
+        commentsCount
+        createdAt
+        artist {
+          name
+          owner
+          avatar
+        }
+        mediaId
+        isLiked
+        alt
       }
-      likesCount
-      sharedCount
-      commentsCount
-      createdAt
-      artist {
-        name
-        owner
-        avatar
-      }
-      mediaId
-      isLiked
-      alt
     }
   }
 `;
