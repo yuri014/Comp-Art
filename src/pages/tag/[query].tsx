f1a4784e168541b925a73ba12893bdedb6c1a396
yuri014 <yuriflores2012@gmail.com>
2021-05-31 19:21:41 -0300

    starting to impl tag page

---
+++ src/pages/tag/[query].tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useQuery } from '@apollo/client';
+
+import Home from '@components/Home';
+import Header from '@components/Header';
+import MobileFooter from '@components/MobileFooter';
+import MobileHeader from '@components/MobileHeader';
+import Post from '@components/Post';
+import ArtistPost from '@components/Post/ArtistPost';
+import LevelContext from '@context/level';
+import { initializeApollo } from '@graphql/apollo/config';
+import { GET_SEARCH_POSTS } from '@graphql/queries/search';
+import { GET_LOGGED_PROFILE } from '@graphql/queries/profile';
+import { Timeline } from '@interfaces/Post';
+import { ILoggedProfile } from '@interfaces/Profile';
+import useGetLevel from '@hooks/getLevel';
+import usePostsMutations from '@hooks/postMutations';
+import HomeContainer from '../home/_styles';
+
+interface SearchPost {
+  searchPost: Array<Timeline>;
+}
+
+const TagPage: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
+  const {
+    query: { query },
+  } = useRouter();
+  const { data, loading } = useQuery<SearchPost>(GET_SEARCH_POSTS, {
+    variables: {
+      query: `#${query}`,
+      offset: 0,
+    },
+  });
+
+  const { getLevel, profileLevel } = useGetLevel();
+
+  return (
+    <HomeContainer>
+      <Head>
+        <title>Comp-Art</title>
+      </Head>
+      {getLoggedProfile && !loading && data && (
+        <>
+          <Header getLoggedProfile={getLoggedProfile} />
+          <LevelContext.Provider
+            value={{ updateLevel: getLevel, level: profileLevel }}
+          >
+            <Home getLoggedProfile={getLoggedProfile}>
+              {data.searchPost.map(post => (
+                <Post useInteractions={usePostsMutations} post={post}>
+                  <ArtistPost post={post} />
+                </Post>
+              ))}
+            </Home>
+            <MobileHeader getLoggedProfile={getLoggedProfile} />
+          </LevelContext.Provider>
+        </>
+      )}
+      <MobileFooter />
+    </HomeContainer>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { jwtToken } = context.req.cookies;
+
+  const client = initializeApollo(null, jwtToken);
+
+  const getProfile = await client.query({
+    query: GET_LOGGED_PROFILE,
+    errorPolicy: 'ignore',
+  });
+
+  const { getLoggedProfile } = getProfile.data;
+
+  if (jwtToken && !getLoggedProfile) {
+    return {
+      redirect: {
+        destination: '/register-profile',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      getLoggedProfile,
+    },
+  };
+};
+
+export default TagPage;
