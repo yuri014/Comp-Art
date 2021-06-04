625f5c005e411d2a133f177117c8cc8171bd327a
yuri014 <yuriflores2012@gmail.com>
2021-06-04 18:18:33 -0300

    creating profile seo schema

---
+++ src/schemas/Profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Person } from 'schema-dts';
+import { JsonLd } from 'react-schemaorg';
+import { IProfile } from '@interfaces/Profile';
+
+interface ProfileSchemaProps {
+  profile: IProfile;
+}
+
+const ProfileSchema: React.FC<ProfileSchemaProps> = ({ profile }) => (
+  <JsonLd<Person>
+    item={{
+      '@context': 'https://schema.org',
+      '@type': 'Person',
+      name: profile.name,
+      alternateName: profile.owner,
+      knowsAbout: profile.hashtags,
+      description: profile.bio,
+      image: process.env.NEXT_PUBLIC_API_HOST + profile.avatar,
+      sameAs: [
+        `https://www.facebook.com/${profile.links.facebook}`,
+        `https://twitter.com/${profile.links.twitter}`,
+        `https://pinterest.com/${profile.links.pinterest}`,
+        `https://soundcloud.com/${profile.links.soundcloud}`,
+        `https://deviantart.com/${profile.links.deviantart}`,
+        `https://wattpad.com/user/${profile.links.wattpad}`,
+        `https://${profile.links.bandcamp}.bandcamp.com/`,
+      ],
+    }}
+  />
+);
+
+export default ProfileSchema;
