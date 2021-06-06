f6c83fce29ae9de62d8948c49298338392dc7aa3
yuri014 <yuriflores2012@gmail.com>
2021-06-06 19:53:08 -0300

    adding custom draft editor style

--- src/components/CreatePost/styles.ts
+++ src/components/CreatePost/styles.ts
@@ -8,64 +8,6 @@ const CreatePostContainer = styled.div`
   justify-content: space-between;
   padding: 2rem 1rem;
 
-  /* Custom mention popover */
-  .mnw6qvm {
-    color: ${({ theme }) => theme.colors.themeColor};
-    text-decoration: none;
-    background-color: ${({ theme }) => theme.colors.popoverBackground};
-    border: none;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-
-  .m6zwb4v {
-    color: ${({ theme }) => theme.colors.pink};
-    background-color: transparent;
-  }
-
-  .mention {
-    padding: 0.5rem 1rem;
-    font-size: 1.4rem;
-    font-weight: 600;
-    transition: filter 0.225s ease-in-out;
-
-    &:hover {
-      background-color: ${({ theme }) => theme.colors.popoverBackground};
-      filter: brightness(1.25);
-    }
-  }
-
-  .mentionSuggestionsEntryContainer {
-    display: flex;
-    align-items: center;
-    width: 100%;
-  }
-
-  .mentionSuggestionsEntryContainerRight {
-    width: 100%;
-    padding-left: 0.8rem;
-  }
-
-  .mentionSuggestionsEntryText,
-  .mentionSuggestionsEntryTitle {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  .mentionSuggestionsEntryTitle {
-    font-size: 1.2rem;
-    font-weight: 300;
-    color: ${({ theme }) => theme.colors.darkGray};
-  }
-
-  .mentionSuggestionsEntryAvatar {
-    display: block;
-    width: 3.2rem;
-    height: 3.2rem;
-    border-radius: 50%;
-  }
-  /* End mention popover */
-
   img {
     width: 5rem;
     height: 5rem;
@@ -166,10 +108,6 @@ const CreatePostContainer = styled.div`
     }
 
     .editor {
-      .hashtag {
-        color: ${({ theme }) => theme.colors.hashtag};
-      }
-
       .DraftEditor-root {
         width: 100%;
         font-size: 1.6rem;
---
+++ src/components/DraftEditor/_styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+const DraftEditorComponent = styled.div`
+  display: contents;
+
+  /* Custom mention popover */
+  .mnw6qvm {
+    color: ${({ theme }) => theme.colors.themeColor};
+    text-decoration: none;
+    background-color: ${({ theme }) => theme.colors.popoverBackground};
+    border: none;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
+
+  .m6zwb4v {
+    color: ${({ theme }) => theme.colors.pink};
+    background-color: transparent;
+  }
+
+  .mention {
+    padding: 0.5rem 1rem;
+    font-size: 1.4rem;
+    font-weight: 600;
+    transition: filter 0.225s ease-in-out;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.popoverBackground};
+      filter: brightness(1.25);
+    }
+  }
+
+  .mentionSuggestionsEntryContainer {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+
+  .mentionSuggestionsEntryContainerRight {
+    width: 100%;
+    padding-left: 0.8rem;
+  }
+
+  .mentionSuggestionsEntryText,
+  .mentionSuggestionsEntryTitle {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .mentionSuggestionsEntryTitle {
+    font-size: 1.2rem;
+    font-weight: 300;
+    color: ${({ theme }) => theme.colors.darkGray};
+  }
+
+  .mentionSuggestionsEntryAvatar {
+    display: block;
+    width: 3.2rem;
+    height: 3.2rem;
+    border-radius: 50%;
+  }
+  /* End mention popover */
+
+  .hashtag {
+    color: ${({ theme }) => theme.colors.hashtag};
+  }
+`;
+
+export default DraftEditorComponent;
