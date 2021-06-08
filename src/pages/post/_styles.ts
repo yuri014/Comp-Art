a0cc26a3e452040994fa65f9fcea173b35a7e589
yuri014 <yuriflores2012@gmail.com>
2021-06-08 00:16:19 -0300

    adding min height

--- src/pages/post/_styles.ts
+++ src/pages/post/_styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const PostPageContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
+  min-height: 100vh;
 
   header {
     nav {
