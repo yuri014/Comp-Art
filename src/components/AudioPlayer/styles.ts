d6d519bf453165c5d00d6dedd7531bce9fd94b0f
yuri014 <yuriflores2012@gmail.com>
2021-06-09 20:54:13 -0300

    removing slider

--- src/components/AudioPlayer/styles.ts
+++ src/components/AudioPlayer/styles.ts
@@ -29,40 +29,6 @@ const AudioPlayerContainer = styled.div`
         }
       }
     }
-
-    .slider {
-      appearance: none;
-      width: 50%;
-      display: block;
-      margin: 3rem auto;
-      height: 1rem;
-      border-radius: 5px;
-      background: ${({ theme }) => theme.colors.gray};
-      outline: none;
-      opacity: 0.7;
-      transition: opacity 0.2s;
-    }
-
-    .slider:hover {
-      opacity: 1;
-    }
-
-    .slider::-webkit-slider-thumb {
-      appearance: none;
-      width: 25px;
-      height: 25px;
-      border-radius: 50%;
-      background: ${({ theme }) => theme.colors.mainColor};
-      cursor: pointer;
-    }
-
-    .slider::-moz-range-thumb {
-      width: 25px;
-      height: 25px;
-      border-radius: 50%;
-      background: ${({ theme }) => theme.colors.mainColor};
-      cursor: pointer;
-    }
   }
 `;
 
