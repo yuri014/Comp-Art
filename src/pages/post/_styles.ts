3d5a110b2a31201b20a4287a34d300c846e57104
yuri014 <yuriflores2012@gmail.com>
2021-06-13 15:26:42 -0300

    removing style for split component

--- src/pages/post/_styles.ts
+++ src/pages/post/_styles.ts
@@ -4,25 +4,6 @@ const PostPageContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
   min-height: 100vh;
 
-  header {
-    nav {
-      height: 5rem;
-      font-size: 2rem;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      color: ${({ theme }) => theme.colors.mainColor};
-      padding: 0 calc(1rem - 6px);
-      width: 100%;
-      background: ${({ theme }) => theme.colors.alternativeBackground};
-      border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
-
-      a {
-        color: ${({ theme }) => theme.colors.mainColor};
-      }
-    }
-  }
-
   main {
     .profile {
       padding: 1rem calc(2rem - 6px);
