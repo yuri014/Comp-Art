aa2e7e4c7ff190990c4b2c31a8e8afeea693db52
yuri014 <yuriflores2012@gmail.com>
2021-06-18 23:19:24 -0300

    adding code input form

--- src/components/CodeInputModal/index.tsx
+++ src/components/CodeInputModal/index.tsx
@@ -1,9 +1,12 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import ReactCodeInput from 'react-verification-code-input';
+import { useMutation } from '@apollo/client';
 
 import Modal from '@components/Modal';
 import CAButton from '@styles/components/button';
+import { CONFIRMATION_EMAIL } from '@graphql/mutations/user';
+import { AuthContext } from '@context/auth';
+import CodeInputModalContainer from './styles';
 
 interface CodeInputModalProps {
   email: string;
@@ -16,29 +19,68 @@ const CodeInputModal: React.FC<CodeInputModalProps> = ({
   setShowModal,
   showModal,
 }) => {
-  return (
-    <Modal
-      title="Quase lá, vai ser jogo rápido!"
-      text={
-        <>
-          <p>
-            Já enviamos um código para o seu e-mail <span>{email}</span>
-          </p>
-          <p>
-            Verifique sua caixa de entrada e insira o código no campo abaixo
-            para verificar seu e-mail
-          </p>
-        </>
-      }
-      show={showModal}
-      onHide={() => setShowModal(false)}
-    >
-      <form className="verification-code">
-        <ReactCodeInput fields={4} className="input-group" />
+  const [code, setCode] = useState('');
+  const authContext = useContext(AuthContext);
+  const [sendCode] = useMutation(CONFIRMATION_EMAIL, {
+    onCompleted: response =>
+      authContext.login({ ...response.confirmationEmail }),
+  });
 
-        <CAButton type="submit">ENVIAR</CAButton>
-      </form>
-    </Modal>
+  const handleSubmit = () => {
+    sendCode({
+      variables: {
+        code,
+        email,
+      },
+    });
+  };
+
+  return (
+    <CodeInputModalContainer>
+      <Modal
+        title="Quase lá, vai ser jogo rápido!"
+        text={
+          <>
+            <p>
+              Já enviamos um código para o seu e-mail <span>{email}</span>
+            </p>
+            <p>
+              Verifique sua caixa de entrada e insira o código no campo abaixo
+              para verificar seu e-mail
+            </p>
+          </>
+        }
+        show={showModal}
+        onHide={() => setShowModal(false)}
+      >
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          className="verification-code"
+          autoComplete="off"
+        >
+          <ReactCodeInput
+            fields={4}
+            className="input-group"
+            required
+            onChange={e => setCode(e)}
+          />
+
+          <div className="buttons">
+            <div className="resend-code-container">
+              <p>Pode levar um tempo para o código chegar.</p>
+              <div className="resend-code">
+                <p>Caso não receba: </p>{' '}
+                <button type="button">Reenviar código</button>
+              </div>
+            </div>
+            <CAButton type="submit">ENVIAR</CAButton>
+          </div>
+        </form>
+      </Modal>
+    </CodeInputModalContainer>
   );
 };
 
---
+++ src/components/CodeInputModal/styles.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+const CodeInputModalContainer = styled.div`
+  .modal-body {
+    p {
+      text-align: left;
+
+      span {
+        color: ${({ theme }) => theme.colors.mainColor};
+      }
+
+      & + p {
+        margin-top: 1rem;
+      }
+    }
+  }
+
+  .verification-code {
+    box-shadow: none;
+    justify-content: center;
+    padding: 0;
+    margin-top: 0;
+
+    .input-group {
+      margin-bottom: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100% !important; /* Sobrescrever inline css da lib */
+
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+      }
+
+      input {
+        width: 6rem;
+        height: 6rem;
+        background-color: ${({ theme }) => theme.colors.inputBackground};
+        border: none;
+        border-radius: 5px;
+        font-size: 3rem;
+        text-align: center;
+        color: ${({ theme }) => theme.colors.themeColor};
+
+        &:focus {
+          border: 2px solid ${({ theme }) => theme.colors.danger};
+          outline: none;
+        }
+      }
+    }
+
+    button {
+      display: block;
+      margin: 0 auto;
+      width: 90%;
+      margin-bottom: 2rem;
+      color: ${({ theme }) => theme.colors.modalBackrgound};
+    }
+
+    .buttons {
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: center;
+      justify-content: space-between;
+
+      .resend-code-container {
+        font-size: 1.3rem;
+        margin-bottom: 2rem;
+
+        .resend-code {
+          p {
+            display: inline;
+          }
+
+          button {
+            display: contents;
+            color: ${({ theme }) => theme.colors.mainColor};
+          }
+        }
+      }
+    }
+  }
+
+  @media (min-width: 1100px) {
+    .verification-code {
+      .input-group {
+        div {
+          gap: 2rem;
+        }
+      }
+
+      button {
+        width: 70%;
+        margin-bottom: 0;
+      }
+    }
+  }
+`;
+
+export default CodeInputModalContainer;
