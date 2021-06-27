9c70bb336a378438016e3e693899d8624e710f86
yuri014 <yuriflores2012@gmail.com>
2021-06-27 20:45:04 -0300

    adding timeline

--- src/pages/tag/[query].tsx
+++ src/pages/tag/[query].tsx
@@ -2,38 +2,23 @@ import React from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useQuery } from '@apollo/client';
 
 import Home from '@components/Home';
 import Header from '@components/Header';
 import MobileFooter from '@components/MobileFooter';
 import MobileHeader from '@components/MobileHeader';
-import Post from '@components/Post';
-import ArtistPost from '@components/Post/ArtistPost';
-import LoadingPost from '@components/Post/LoadingPost';
+import Timeline from '@components/Timeline';
 import { LevelProvider } from '@context/level';
 import { initializeApollo } from '@graphql/apollo/config';
 import { GET_SEARCH_POSTS } from '@graphql/queries/search';
 import { GET_LOGGED_PROFILE } from '@graphql/queries/profile';
-import { Timeline } from '@interfaces/Post';
 import { ILoggedProfile } from '@interfaces/Profile';
-import usePostsMutations from '@hooks/postMutations';
 import HomeContainer from '../home/_styles';
-
-interface SearchPost {
-  searchPost: Array<Timeline>;
-}
 
 const TagPage: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
   const {
     query: { query },
   } = useRouter();
-  const { data, loading } = useQuery<SearchPost>(GET_SEARCH_POSTS, {
-    variables: {
-      query: `#${query}`,
-      offset: 0,
-    },
-  });
 
   return (
     <HomeContainer>
@@ -45,15 +30,11 @@ const TagPage: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
           <Header getLoggedProfile={getLoggedProfile} />
           <LevelProvider>
             <Home getLoggedProfile={getLoggedProfile}>
-              {loading || data.searchPost.length === 0 ? (
-                <LoadingPost loading={loading} />
-              ) : (
-                data.searchPost.map(post => (
-                  <Post useInteractions={usePostsMutations} post={post}>
-                    <ArtistPost post={post} />
-                  </Post>
-                ))
-              )}
+              <Timeline
+                query={GET_SEARCH_POSTS}
+                queryName="searchPost"
+                otherVariables={{ query: `#${query}` }}
+              />
             </Home>
             <MobileHeader getLoggedProfile={getLoggedProfile} />
           </LevelProvider>
