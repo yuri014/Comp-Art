b269f0b935e2ecc704d3e519f4f26c0bae26c114
yuri014 <yuriflores2012@gmail.com>
2021-06-29 20:36:53 -0300

    adding scope provider to update theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import Link from 'next/link';
+import { ThemeProvider } from '@material-ui/core';
 import { FaEnvelopeOpenText, FaMoon, FaSignInAlt, FaSun } from 'react-icons/fa';
 import { BiDonateHeart } from 'react-icons/bi';
 
 import NotificationMenu from '@components/Notification/NotificationMenu';
 import SearchInput from '@components/SearchInput';
+import mainDarkTheme from '@styles/themes/MainDarkTheme';
+import mainLightTheme from '@styles/themes/MainLightTheme';
 import { HeaderContainer } from './styles';
 import { AuthContext } from '../../context/auth';
 import ThemeContext from '../../context/theme';
@@ -17,55 +20,57 @@ const Header: React.FC<ILoggedProfile> = ({ getLoggedProfile }) => {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <HeaderContainer>
-      <Link href="/home">
-        <a>
-          <Logo />
-        </a>
-      </Link>
-      <SearchInput />
-      {auth.user ? (
-        <>
+    <ThemeProvider theme={isDarkMode ? mainDarkTheme : mainLightTheme}>
+      <HeaderContainer>
+        <Link href="/home">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+        <SearchInput />
+        {auth.user ? (
+          <>
+            <div className="header-icons">
+              <button type="button">
+                <FaEnvelopeOpenText />
+              </button>
+              <a
+                href="https://www.catarse.me/compart_6d8c?ref=project_link"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Apoiar Comp-Art"
+              >
+                <BiDonateHeart />
+              </a>
+              <NotificationMenu />
+              <ProfileMenu
+                isDarkMode={isDarkMode}
+                toggleTheme={toggleTheme}
+                getLoggedProfile={getLoggedProfile}
+                logout={auth.logout}
+              />
+            </div>
+          </>
+        ) : (
           <div className="header-icons">
-            <button type="button">
-              <FaEnvelopeOpenText />
-            </button>
-            <a
-              href="https://www.catarse.me/compart_6d8c?ref=project_link"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Apoiar Comp-Art"
+            <span />
+            <span />
+            <button
+              type="button"
+              aria-label={`Mudar para modo ${!isDarkMode ? 'Escuro' : 'Claro'}`}
+              onClick={() => toggleTheme()}
             >
-              <BiDonateHeart />
-            </a>
-            <NotificationMenu />
-            <ProfileMenu
-              isDarkMode={isDarkMode}
-              toggleTheme={toggleTheme}
-              getLoggedProfile={getLoggedProfile}
-              logout={auth.logout}
-            />
+              {!isDarkMode ? <FaMoon /> : <FaSun />}
+            </button>
+            <Link href="/login">
+              <a title="Ir para o login">
+                <FaSignInAlt />
+              </a>
+            </Link>
           </div>
-        </>
-      ) : (
-        <div className="header-icons">
-          <span />
-          <span />
-          <button
-            type="button"
-            aria-label={`Mudar para modo ${!isDarkMode ? 'Escuro' : 'Claro'}`}
-            onClick={() => toggleTheme()}
-          >
-            {!isDarkMode ? <FaMoon /> : <FaSun />}
-          </button>
-          <Link href="/login">
-            <a title="Ir para o login">
-              <FaSignInAlt />
-            </a>
-          </Link>
-        </div>
-      )}
-    </HeaderContainer>
+        )}
+      </HeaderContainer>
+    </ThemeProvider>
   );
 };
 
