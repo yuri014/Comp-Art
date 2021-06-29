f16f8a840211ed41092cc477d03b92a313444140
yuri014 <yuriflores2012@gmail.com>
2021-06-29 00:36:46 -0300

    adding scope provider to update theme

--- src/components/MobileFooter/index.tsx
+++ src/components/MobileFooter/index.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FaBell, FaCog, FaHome, FaSearch } from 'react-icons/fa';
 import { useRouter } from 'next/dist/client/router';
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  ThemeProvider,
+} from '@material-ui/core';
 
+import ThemeContext from '@context/theme';
+import mainDarkTheme from '@styles/themes/MainDarkTheme';
+import mainLightTheme from '@styles/themes/MainLightTheme';
 import MobileFooterContainer from './styles';
 
 const MobileFooter: React.FC = () => {
   const routes = useRouter();
-
+  const { isDarkMode } = useContext(ThemeContext);
   const [value, setValue] = useState(routes.pathname);
 
   const handleChange = (
@@ -19,33 +26,35 @@ const MobileFooter: React.FC = () => {
 
   return (
     <MobileFooterContainer>
-      <BottomNavigation value={value} onChange={handleChange}>
-        <BottomNavigationAction
-          label="Home"
-          value="/home"
-          icon={<FaHome />}
-          onClick={() => routes.push('/home')}
-        />
+      <ThemeProvider theme={isDarkMode ? mainDarkTheme : mainLightTheme}>
+        <BottomNavigation value={value} onChange={handleChange}>
+          <BottomNavigationAction
+            label="Home"
+            value="/home"
+            icon={<FaHome />}
+            onClick={() => routes.push('/home')}
+          />
 
-        <BottomNavigationAction
-          label="Search"
-          value="/search"
-          icon={<FaSearch />}
-          onClick={() => routes.push('/search')}
-        />
-        <BottomNavigationAction
-          label="Notifications"
-          value="/notifications"
-          icon={<FaBell />}
-          onClick={() => routes.push('/notifications')}
-        />
-        <BottomNavigationAction
-          label="Quest"
-          value="/quest"
-          icon={<FaCog />}
-          onClick={() => routes.push('/config')}
-        />
-      </BottomNavigation>
+          <BottomNavigationAction
+            label="Search"
+            value="/search"
+            icon={<FaSearch />}
+            onClick={() => routes.push('/search')}
+          />
+          <BottomNavigationAction
+            label="Notifications"
+            value="/notifications"
+            icon={<FaBell />}
+            onClick={() => routes.push('/notifications')}
+          />
+          <BottomNavigationAction
+            label="Quest"
+            value="/quest"
+            icon={<FaCog />}
+            onClick={() => routes.push('/config')}
+          />
+        </BottomNavigation>
+      </ThemeProvider>
     </MobileFooterContainer>
   );
 };
