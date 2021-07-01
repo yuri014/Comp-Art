53131f9209157251f4fba8c55bd81fb15ee3c242
yuri014 <yuriflores2012@gmail.com>
2021-06-30 21:25:41 -0300

    adding share with description component

---
+++ src/components/Post/Buttons/ShareButton/ModalShareDescription.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import DraftEditor from '@components/DraftEditor';
+import DescriptionCounter from '@components/DraftEditor/utils/DescriptionCounter';
+import usePreventMemoryLeak from '@hooks/preventMemoryLeak';
+import CAButton from '@styles/components/button';
+import { ModalShareContainer } from '../styles';
+
+interface ModalShareDescriptionProps {
+  sharePost: (description?: string) => void;
+}
+
+const ModalShareDescription: React.FC<ModalShareDescriptionProps> = ({
+  sharePost,
+}) => {
+  const isMount = usePreventMemoryLeak();
+
+  const [commentField, setCommentField] = useState('');
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <>
+      {isMount && (
+        <ModalShareContainer className="modal-content">
+          <div className="input-container">
+            <div className="draft-container">
+              <DraftEditor
+                setText={setCommentField}
+                setProgress={setProgress}
+                limit={1200}
+                placeholder="Digite aqui o seu comentário..."
+              />
+            </div>
+            <DescriptionCounter
+              className="counter-container"
+              progress={progress}
+            />
+          </div>
+          <CAButton onClick={() => sharePost(commentField)} type="button">
+            Compartilhar
+          </CAButton>
+        </ModalShareContainer>
+      )}
+    </>
+  );
+};
+
+export default ModalShareDescription;
