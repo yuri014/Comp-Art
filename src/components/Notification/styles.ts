943f5da25fac9d63fa70fa1f27b39bb0121d1ec0
yuri014 <yuriflores2012@gmail.com>
2021-07-02 23:37:12 -0300

    change notification menu box shadow

--- src/components/Notification/styles.ts
+++ src/components/Notification/styles.ts
@@ -91,7 +91,5 @@ export const NotificationMenuContainer = styled.div`
   max-height: 22rem;
   overflow: auto;
   border-radius: 5px;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
 `;
