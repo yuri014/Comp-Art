484d22521053665edac1789adfbed2268b6e5f91
yuri014 <yuriflores2012@gmail.com>
2021-07-10 16:32:27 -0300

    fix ssr header

--- src/components/NonAuthAltHeader/index.tsx
+++ src/components/NonAuthAltHeader/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { FaArrowLeft, FaMoon, FaSun } from 'react-icons/fa';
-import { IconButton } from '@material-ui/core';
+import { IconButton, NoSsr } from '@material-ui/core';
 
 import { IThemeContext } from '@context/theme';
 import NonAuthAltHeaderContainer from './styles';
@@ -13,25 +13,27 @@ const NonAuthAltHeader: React.FC<IThemeContext> = ({
   const router = useRouter();
 
   return (
-    <NonAuthAltHeaderContainer>
-      <nav>
-        <IconButton
-          onClick={() => router.back()}
-          aria-label="Voltar"
-          color="secondary"
-        >
-          <FaArrowLeft />
-        </IconButton>
-        <IconButton
-          color="secondary"
-          type="button"
-          aria-label={`Mudar para modo ${!isDarkMode ? 'Escuro' : 'Claro'}`}
-          onClick={() => toggleTheme()}
-        >
-          {!isDarkMode ? <FaMoon /> : <FaSun />}
-        </IconButton>
-      </nav>
-    </NonAuthAltHeaderContainer>
+    <NoSsr>
+      <NonAuthAltHeaderContainer>
+        <nav>
+          <IconButton
+            onClick={() => router.back()}
+            aria-label="Voltar"
+            color="secondary"
+          >
+            <FaArrowLeft />
+          </IconButton>
+          <IconButton
+            color="secondary"
+            type="button"
+            aria-label={`Mudar para modo ${!isDarkMode ? 'Escuro' : 'Claro'}`}
+            onClick={() => toggleTheme()}
+          >
+            {!isDarkMode ? <FaMoon /> : <FaSun />}
+          </IconButton>
+        </nav>
+      </NonAuthAltHeaderContainer>
+    </NoSsr>
   );
 };
 
