46b0077688b3e062e43fbd8c1c6e910392b9a6aa
yuri014 <yuriflores2012@gmail.com>
2021-07-10 21:43:55 -0300

    returning empty when there is no src

--- src/functions/getBase64Image.ts
+++ src/functions/getBase64Image.ts
@@ -3,6 +3,10 @@ import { getPlaiceholder } from 'plaiceholder';
 const getBase64Image = async (getSrc: () => string): Promise<string> => {
   const src = getSrc();
 
+  if (src === '') {
+    return src;
+  }
+
   const { base64 } = await getPlaiceholder(src, {
     size: 10,
   });
