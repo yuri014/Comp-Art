67b5d3d1848ddef33b64c7d64248d66aa56227aa
yuri014 <yuriflores2012@gmail.com>
2021-07-12 22:56:52 -0300

    removing log

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -76,10 +76,7 @@ const Timeline: React.FC<TimelineProps> = ({
   return (
     <>
       {loading || error || data[`${queryName}`].length === 0 ? (
-        <>
-          {console.log(data)}
-          <LoadingPost loading={loading} />
-        </>
+        <LoadingPost loading={loading} />
       ) : (
         <TimelineManager
           lastPostRef={lastPostRef}
