3652d7c08968dbfdc3d8e0d9fee519c0ac0dd34b
yuri014 <yuriflores2012@gmail.com>
2021-07-15 01:15:38 -0300

    adding config for tests

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
+  setupFiles: ['@testing-library/react/dont-cleanup-after-each'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  bail: 1,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['components/**/*.tsx', 'pages/**/*.tsx'],
+  coverageReporters: ['lcov', 'text'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript',
+    },
+  },
+  moduleNameMapper: {
+    '@components/(.*)': '<rootDir>/src/$1',
+  },
+  moduleNameMapper: {
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@context/(.*)': '<rootDir>/src/context/$1',
+    '@graphql/(.*)': '<rootDir>/src/graphql/$1',
+    '@hooks/(.*)': '<rootDir>/src/hooks/$1',
+    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@validations/(.*)': '<rootDir>/src/validations/$1',
+    '@interfaces/(.*)': '<rootDir>/src/interfaces/$1',
+    '@ssr-functions/(.*)': '<rootDir>/src/functions/$1',
+  },
+};
