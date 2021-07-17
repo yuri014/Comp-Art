3f186c09f03d3d8844184299d2611f9182d24b13
yuri014 <yuriflores2012@gmail.com>
2021-07-17 15:21:20 -0300

    validating null post

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -1,45 +1,52 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 
-import { PostProps } from '@interfaces/Post';
+import { ErrorPost, PostProps } from '@interfaces/Post';
 import AuthorInfo from '@components/Post/utils/AuthorInfo';
 import ArtistPost from '@components/Post/ArtistPost';
 import { DELETE_SHARE } from '@graphql/mutations/share';
 import TextBox from '@components/TextBox';
 import ShareContainer from './styles';
+import NullPost from './NullPost';
 
 const Share: React.FC<PostProps> = ({ post }) => {
   const [isDeleted, setIsDeleted] = useState(false);
   const [deleteShare] = useMutation(DELETE_SHARE);
 
+  const isNullPost = (post.post as unknown) as ErrorPost;
+
   return (
     <>
       {!isDeleted && (
         <ShareContainer>
-          <div className="share">
-            <div className="share-info">
-              <AuthorInfo
-                createdAt={post.createdAt}
-                postID={post._id}
-                profile={post.post.artist}
-                handleDeletePost={() => {
-                  deleteShare({ variables: { id: post._id } });
-                  setIsDeleted(true);
-                }}
-              />
+          {isNullPost.error ? (
+            <NullPost />
+          ) : (
+            <div className="share">
+              <div className="share-info">
+                <AuthorInfo
+                  createdAt={post.createdAt}
+                  postID={post._id}
+                  profile={post.post.artist}
+                  handleDeletePost={() => {
+                    deleteShare({ variables: { id: post._id } });
+                    setIsDeleted(true);
+                  }}
+                />
+              </div>
+              <div className="share-content">
+                {post.post.description && (
+                  <div className="description">
+                    <TextBox text={post.post.description} />
+                  </div>
+                )}
+                <ArtistPost
+                  isShare
+                  post={{ ...post.post, imageHeight: post.imageHeight }}
+                />
+              </div>
             </div>
-            <div className="share-content">
-              {post.post.description && (
-                <div className="description">
-                  <TextBox text={post.post.description} />
-                </div>
-              )}
-              <ArtistPost
-                isShare
-                post={{ ...post.post, imageHeight: post.imageHeight }}
-              />
-            </div>
-          </div>
+          )}
         </ShareContainer>
       )}
     </>
