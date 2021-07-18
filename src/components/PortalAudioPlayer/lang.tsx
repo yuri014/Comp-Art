4792eedaa7b5d249c13030628f3bbddb86207ee7
yuri014 <yuriflores2012@gmail.com>
2021-07-17 23:11:01 -0300

    langs for player

---
+++ src/components/PortalAudioPlayer/lang.tsx
@@ -0,0 +1,34 @@
+import { FaHeadphones } from 'react-icons/fa';
+
+const customLocale = {
+  playModeText: {
+    order: 'Ordem',
+    orderLoop: 'Repetir todas as faixas',
+    singleLoop: 'Repetir faixa atual',
+    shufflePlay: 'Modo aleatório',
+  },
+  openText: 'Abrir',
+  closeText: 'Minimizar',
+  emptyText: 'Vazio',
+  clickToPlayText: 'Tocar',
+  clickToPauseText: 'Pausar',
+  nextTrackText: 'Próxima faixa',
+  previousTrackText: 'Faixa Anterior',
+  reloadText: 'Voltar ao ínicio',
+  volumeText: 'Volume',
+  playListsText: 'Playlist',
+  toggleLyricText: 'toggleLyricText',
+  toggleMiniModeText: 'Minimizar',
+  destroyText: 'destroyText',
+  downloadText: 'downloadText',
+  lightThemeText: 'L',
+  darkThemeText: 'D',
+  switchThemeText: 'Trocar tema',
+  removeAudioListsText: 'Limpar playlist',
+  emptyLyricText: 'emptyLyricText',
+  clickToDeleteText: (name: string): string => `Deletar: ${name}`,
+  // eslint-disable-next-line react/react-in-jsx-scope
+  controllerTitle: <FaHeadphones />,
+};
+
+export default customLocale;
