df89a7c7a26b24841c0ca4b59e77b84aa35d0e98
yuri014 <yuriflores2012@gmail.com>
2021-07-18 00:46:04 -0300

    adding label to make upload thumbnail easier

--- src/components/CreatePost/utils/styles.ts
+++ src/components/CreatePost/utils/styles.ts
@@ -19,37 +19,63 @@ const MediaFormContainer = styled.div`
     }
   }
 
-  .media {
-    button {
-      cursor: pointer;
-      top: 4px;
-      right: 4px;
-      position: absolute;
-      background-color: rgba(0, 0, 0, 0.9);
-      color: white;
-      border-radius: 50%;
-      padding: 0.7rem;
-      display: grid;
-      place-content: center;
-      border: none;
-      font-size: 1.8rem;
+  .cleaner {
+    cursor: pointer;
+    top: 4px;
+    right: 4px;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.9);
+    color: white;
+    border-radius: 50%;
+    padding: 0.7rem;
+    display: grid;
+    place-content: center;
+    border: none;
+    font-size: 1.8rem;
 
-      &:focus,
-      &:hover {
-        background-color: rgb(35, 40, 43, 0.9);
-        outline: none;
-      }
-
-      &:focus {
-        border: 2px solid ${({ theme }) => theme.colors.darkGray};
-        padding: calc(0.6rem - 2px);
-      }
+    &:focus,
+    &:hover {
+      background-color: rgb(35, 40, 43, 0.9);
+      outline: none;
     }
+
+    &:focus {
+      border: 2px solid ${({ theme }) => theme.colors.darkGray};
+      padding: calc(0.6rem - 2px);
+    }
+  }
+
+  .media {
+    position: relative;
 
     img {
       width: 12rem;
       height: 12rem;
       border-radius: 5px;
+    }
+
+    label {
+      position: absolute;
+      bottom: 0.6rem;
+      right: 0.6rem;
+      width: auto;
+      background: ${({ theme }) => theme.colors.mainColorMedium};
+      font-size: 1rem;
+      padding: 0.7rem;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      transition: background-color 0.225s ease-out;
+      box-shadow: ${({ theme }) => theme.colors.mainShadow};
+
+      &:focus,
+      &:hover {
+        border: 2px solid ${({ theme }) => theme.colors.mainColor};
+        padding: calc(0.7rem - 2px);
+        outline: none;
+      }
     }
   }
 
