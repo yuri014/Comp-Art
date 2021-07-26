77f5886de0a3874336eb36957f68a99d082d622c
yuri014 <yuriflores2012@gmail.com>
2021-07-26 18:48:36 -0300

    fixing follow button

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -1,6 +1,8 @@
+import { useMutation } from '@apollo/client';
 import ProfileImage from '@components/ProfileImage';
+import { FOLLOW_PROFILE, UNFOLLOW_PROFILE } from '@graphql/mutations/profile';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UseInfiniteScroll } from '../../hooks/infiniteScroll';
 import { IProfile } from '../../interfaces/Profile';
 import CASecondaryButton from '../../styles/components/secondaryButton';
@@ -11,37 +13,69 @@ interface ProfileSimpleCardProps {
   ref?: UseInfiniteScroll;
 }
 
-const ProfileSimpleCard: React.FC<ProfileSimpleCardProps> = ({ profile }) => (
-  <ProfileSimpleCardContainer>
-    <Link href={`/profile/${profile.owner}`} key={profile.owner}>
-      <a>
-        <ProfileImage
-          avatar={profile.avatar}
-          alt={profile.name}
-          username={profile.owner}
-          className="profile-image"
-        />
-        <div className="profile-content">
-          <div className="first-row">
-            <strong>{profile.name}</strong>
-            {profile.followsYou && (
-              <div className="follows-you">Segue você</div>
-            )}
+const ProfileSimpleCard: React.FC<ProfileSimpleCardProps> = ({ profile }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const [follow] = useMutation(FOLLOW_PROFILE, {
+    variables: { username: profile.owner },
+  });
+  const [unfollow] = useMutation(UNFOLLOW_PROFILE, {
+    variables: { username: profile.owner },
+  });
+
+  useEffect(() => {
+    setIsFollowing(profile.isFollowing);
+  }, [profile.isFollowing]);
+
+  return (
+    <ProfileSimpleCardContainer>
+      <Link href={`/profile/${profile.owner}`} key={profile.owner}>
+        <a>
+          <ProfileImage
+            avatar={profile.avatar}
+            alt={profile.name}
+            username={profile.owner}
+            className="profile-image"
+          />
+          <div className="profile-content">
+            <div className="first-row">
+              <strong>{profile.name}</strong>
+              {profile.followsYou && (
+                <div className="follows-you">Segue você</div>
+              )}
+            </div>
+            <div className="second-row">
+              <p>@{profile.owner}</p>
+              <p className="bio">{profile.bio}</p>
+            </div>
           </div>
-          <div className="second-row">
-            <p>@{profile.owner}</p>
-            <p className="bio">{profile.bio}</p>
-          </div>
-        </div>
-      </a>
-    </Link>
-    <CASecondaryButton
-      className={`main-color ${profile.isFollowing && 'active'}`}
-      type="button"
-    >
-      {profile.isFollowing ? 'SEGUINDO' : 'SEGUIR'}
-    </CASecondaryButton>
-  </ProfileSimpleCardContainer>
-);
+        </a>
+      </Link>
+      {isFollowing ? (
+        <CASecondaryButton
+          type="button"
+          className={`main-color ${profile.isFollowing && 'active'}`}
+          onClick={() => {
+            unfollow();
+            setIsFollowing(false);
+          }}
+        >
+          SEGUINDO
+        </CASecondaryButton>
+      ) : (
+        <CASecondaryButton
+          type="button"
+          className={`main-color ${profile.isFollowing && 'active'}`}
+          onClick={() => {
+            follow();
+            setIsFollowing(true);
+          }}
+        >
+          SEGUIR
+        </CASecondaryButton>
+      )}
+    </ProfileSimpleCardContainer>
+  );
+};
 
 export default ProfileSimpleCard;
