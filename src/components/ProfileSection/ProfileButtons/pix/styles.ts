5618a4e575fc0825ae57f3d2005d544feca386b3
yuri014 <yuriflores2012@gmail.com>
2021-08-02 20:17:59 -0300

    pix qrcode styles

---
+++ src/components/ProfileSection/ProfileButtons/pix/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CreatePixContainer = styled.form`
+  padding: 0 2rem;
+`;
+
+export const RenderPixQrCodeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
