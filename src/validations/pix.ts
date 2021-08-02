fa62f16bf498a54150c40059e616937538e4064a
yuri014 <yuriflores2012@gmail.com>
2021-08-02 20:15:20 -0300

    adding pix validation

---
+++ src/validations/pix.ts
@@ -0,0 +1,24 @@
+import * as yup from 'yup';
+
+yup.setLocale({
+  mixed: {
+    required: 'Preencha esse campo para continuar',
+  },
+  string: {
+    email: 'Preencha um e-mail válido',
+    min: 'Valor muito curto (mínimo ${min} caracteres)',
+    max: 'Valor muito longo (máximo ${max} caracteres)',
+  },
+  number: {
+    min: 'Valor inválido (deve ser maior ou igual a ${min})',
+    max: 'Valor inválido (deve ser menor ou igual a ${max})',
+  },
+});
+
+const pixSchema = yup.object().shape({
+  key: yup.string().min(2).max(255).required(),
+  message: yup.string().max(72),
+  city: yup.string().min(2).max(255).required(),
+});
+
+export default pixSchema;
