872a6898752e77b87f9a5f7aaf9fb1b86c23e3ea
yuri014 <yuriflores2012@gmail.com>
2021-08-03 00:34:25 -0300

    adding pix copy code

--- src/components/ProfileSection/ProfileButtons/pix/RenderPixQrCode.tsx
+++ src/components/ProfileSection/ProfileButtons/pix/RenderPixQrCode.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { QrCodePix } from 'qrcode-pix';
+import { IoCopy } from 'react-icons/io5';
 
 import { IPixInput } from '@interfaces/Profile';
 import { ModalProvider } from '@context/modal';
+import useCopyToClipboard from '@hooks/copyToClipboard';
+import CAButton from '@styles/components/button';
 import { RenderPixQrCodeContainer } from './styles';
 
 interface RenderPixQrCodeProps {
@@ -12,17 +15,21 @@ interface RenderPixQrCodeProps {
 
 const RenderPixQrCode: React.FC<RenderPixQrCodeProps> = ({ pix, name }) => {
   const [qrCode, setQCode] = useState('');
+  const [copyPastePix, setCopyPastePix] = useState('');
   const [openModal, setOpenModal] = useState(false);
+
+  const { CopyToClipboard, copyToClipboard } = useCopyToClipboard(copyPastePix);
 
   useEffect(() => {
     if (pix.key) {
-      QrCodePix({
+      const pixCode = QrCodePix({
         version: '01',
         name,
         ...pix,
-      })
-        .base64()
-        .then(img => setQCode(img));
+      });
+
+      setCopyPastePix(pixCode.payload);
+      pixCode.base64().then(img => setQCode(img));
     }
   }, [name, pix]);
 
@@ -45,12 +52,22 @@ const RenderPixQrCode: React.FC<RenderPixQrCodeProps> = ({ pix, name }) => {
           title={`Contribua com ${name}`}
           text={
             <p>
-              Aponte seu leitor de QR Code e contribua com a arte de {name}.
+              Aponte seu leitor de QR Code ou copie o código pix e contribua com
+              a arte de <span className="primary-icon">{name}</span>.
             </p>
           }
         >
           <RenderPixQrCodeContainer>
             <img src={qrCode} alt="QR Code" width="300px" height="300px" />
+            <CAButton
+              className="copy-button"
+              type="button"
+              onClick={copyToClipboard}
+            >
+              Copiar &quot;Pix Copia e Cola&quot;
+              <IoCopy />
+            </CAButton>
+            <CopyToClipboard />
           </RenderPixQrCodeContainer>
         </ModalProvider>
       )}
