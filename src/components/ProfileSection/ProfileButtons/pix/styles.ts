2a76d91f4908e054c4c620b6496d173edacb7c06
yuri014 <yuriflores2012@gmail.com>
2021-08-03 00:34:19 -0300

    styles for copy button

--- src/components/ProfileSection/ProfileButtons/pix/styles.ts
+++ src/components/ProfileSection/ProfileButtons/pix/styles.ts
@@ -7,5 +7,15 @@ export const CreatePixContainer = styled.form`
 export const RenderPixQrCodeContainer = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
+
+  .copy-button {
+    width: 22rem;
+    display: inherit;
+    align-items: inherit;
+    justify-content: inherit;
+    gap: 0.4rem;
+    margin-top: 2rem;
+  }
 `;
